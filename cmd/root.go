fdc6fd9cd7c280c4e4260e0913c6f6e0becfdc0e
Adriano Krauthein <adriano.krauthein@azion.com>
2021-12-06 15:56:07 -0300

    Removing comments

--- cmd/root.go
+++ cmd/root.go
@@ -44,8 +44,6 @@ func tokenLoadFromConf() string {
 	return ""
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
