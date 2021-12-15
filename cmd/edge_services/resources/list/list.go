89079c76fd58bede6e089ee49648b838f5f75018
Francisco Miamoto <francisco.miamoto@azion.com>
2021-12-15 16:07:42 -0300

    feat: add options for list commands

--- cmd/edge_services/resources/list/list.go
+++ cmd/edge_services/resources/list/list.go
@@ -12,7 +12,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ListOptions struct {
+	Limit int64
+	Page  int64
+	// FIXME: ENG-17161
+	SortDesc bool
+	Filter   string
+}
+
 func NewCmd(f *cmdutil.Factory) *cobra.Command {
+	opts := &ListOptions{}
+
 	// listCmd represents the list command
 	listCmd := &cobra.Command{
 		Use:           "list",
@@ -21,7 +31,6 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-
 			if len(args) < 1 {
 				return utils.ErrorMissingServiceIdArgument
 			}
@@ -36,20 +45,30 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 				return err
 			}
 
-			if err := listAllResources(client, f.IOStreams.Out, ids[0]); err != nil {
+			if err := listAllResources(client, f.IOStreams.Out, opts, ids[0]); err != nil {
 				return err
 			}
 			return nil
 		},
 	}
+
+	listCmd.Flags().Int64Var(&opts.Limit, "limit", 10, "Maximum number of items to fetch (default 10)")
+	listCmd.Flags().Int64Var(&opts.Page, "page", 1, "Select the page from results (default 1)")
+	listCmd.Flags().StringVar(&opts.Filter, "filter", "", "Filter results by their name")
+
 	return listCmd
 }
 
-func listAllResources(client *sdk.APIClient, out io.Writer, service_id int64) error {
+func listAllResources(client *sdk.APIClient, out io.Writer, opts *ListOptions, service_id int64) error {
 	c := context.Background()
 	api := client.DefaultApi
 
-	resp, httpResp, err := api.GetResources(c, service_id).Execute()
+	resp, httpResp, err := api.GetResources(c, service_id).
+		Page(opts.Page).
+		Limit(opts.Limit).
+		Filter(opts.Filter).
+		Execute()
+
 	if err != nil {
 		if httpResp.StatusCode >= 500 {
 			return utils.ErrorInternalServerError
