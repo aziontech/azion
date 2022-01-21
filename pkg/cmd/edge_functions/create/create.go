f93255a28f2c82b7019bf2f0665d2a4fec6b2e88
Francisco Miamoto <francisco.miamoto@azion.com>
2022-01-21 16:53:23 -0300

    feat: add edge_functions module skeleton

---
+++ pkg/cmd/edge_functions/create/create.go
@@ -0,0 +1,27 @@
+package create
+
+import (
+	"errors"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func NewCmd(f *cmdutil.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "create [flags]",
+		Short:         "Create a new Edge Function",
+		Long:          "Create a new Edge Function",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Example: heredoc.Doc(`
+        $ azioncli edge_function create -–name myfunc -–language javascript –-code ./mycode/function.js  -–state active --initiator-type edge-application
+        `),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return errors.New("IMPLEMENT ME")
+		},
+	}
+
+	return cmd
+}
---
+++ pkg/cmd/edge_functions/update/update.go
@@ -0,0 +1,27 @@
+package update
+
+import (
+	"errors"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func NewCmd(f *cmdutil.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "update [flags] <edge_function_id>",
+		Short:         "Update an Edge Function",
+		Long:          "Update an Edge Function",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Example: heredoc.Doc(`
+        $ azioncli edge_functions update 4185 –code ./mycode/function.js –args ./mycode/myargs.json
+        `),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return errors.New("IMPLEMENT ME")
+		},
+	}
+
+	return cmd
+}
