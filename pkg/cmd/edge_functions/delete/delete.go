1a6faaefde8fa0cf67c0070c52d0d7da4aeb6a3d
Patrick Menoti <patrick.menoti@azion.com>
2022-01-26 16:28:14 -0300

    feat: delete command for edge functions

--- pkg/cmd/edge_functions/delete/delete.go
+++ pkg/cmd/edge_functions/delete/delete.go
@@ -1,7 +1,11 @@
 package delete
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/MakeNowJust/heredoc"
+	api "github.com/aziontech/azion-cli/pkg/api/edge_functions"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -10,14 +14,34 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:           "delete <edge_function_id> [flags]",
 		Short:         "Delete an Edge Function",
-		Long:          "Delete an Edge Function",
+		Long:          "Delete an Edge Function based on a given id",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		Example: heredoc.Doc(`
         $ azioncli edge_functions delete 1234
         `),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return cmd.Help()
+			if len(args) < 1 {
+				return fmt.Errorf("missing edge function id argument")
+			}
+
+			httpClient, err := f.HttpClient()
+			if err != nil {
+				return fmt.Errorf("failed to get http client: %w", err)
+			}
+			client := api.NewClient(httpClient, f.Config.GetString("api_url"), f.Config.GetString("token"))
+
+			ctx := context.Background()
+
+			err = client.Delete(ctx, args[0])
+			if err != nil {
+				return err
+			}
+
+			out := f.IOStreams.Out
+			fmt.Fprintf(out, "Edge Function %s was successfully deleted\n", args[0])
+
+			return nil
 		},
 	}
 
