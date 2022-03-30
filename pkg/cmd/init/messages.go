41633cd237fc8e0039423929c08b1ab63e2d4afd
Patrick Menoti <patrick.menoti@azion.com>
2022-03-30 14:46:28 -0300

    fix typos

--- pkg/cmd/init/messages.go
+++ pkg/cmd/init/messages.go
@@ -3,5 +3,5 @@ package init
 var (
 	msgContentOverridden string = "The contents of your Azion folder will be overridden. Are you sure you want to continue? (yes | no)"
 	msgCmdStopped        string = "Init command stopped"
-	msgCmdSuccess        string = "Template succefully fetched and configured"
+	msgCmdSuccess        string = "Template successfully fetched and configured"
 )
