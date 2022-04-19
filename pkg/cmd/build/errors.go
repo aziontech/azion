92e6a80d20775d28e51caafb8830c0822db12021
Francisco Miamoto <francisco.miamoto@azion.com>
2022-04-19 10:15:14 -0300

    chore: fix typo

--- pkg/cmd/build/errors.go
+++ pkg/cmd/build/errors.go
@@ -6,5 +6,5 @@ var (
 	ErrOpeningConfigFile   = errors.New("Failed to open config.json file")
 	ErrUnmarshalConfigFile = errors.New("Failed to parse config.json file")
 	ErrReadEnvFile         = errors.New("Failed to read build.env file")
-	ErrFailedToRunCommand  = errors.New("Failed to run build step command. Verify if the command is correct and/or check it's output for more details")
+	ErrFailedToRunCommand  = errors.New("Failed to run build step command. Verify if the command is correct and/or check its output for more details")
 )
