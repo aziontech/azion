3190bcf2bc1f270ba56ed7e0f6c6d2ef76ff2066
Adriano Krauthein <adriano.krauthein@azion.com>
2022-05-04 22:00:52 -0300

    utils/helpers tests

---
+++ utils/helpers_test.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/aziontech/azion-cli/pkg/contracts"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCobraCmd(t *testing.T) {
+	t.Run("write json content", func(t *testing.T) {
+		path, _ := GetWorkingDir()
+
+		jsonConf := path + "/azion/azion.json"
+
+		err := os.MkdirAll(filepath.Dir(jsonConf), os.ModePerm)
+
+		var azJsonData contracts.AzionJsonData
+		azJsonData.Name = "Test01"
+		azJsonData.Function.Name = "MyFunc"
+		azJsonData.Function.Active = true
+		azJsonData.Function.File = "myfile.js"
+		azJsonData.Function.Language = "javascript"
+		azJsonData.Function.Id = 476
+
+		WriteAzionJsonContent(&azJsonData)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("read json content", func(t *testing.T) {
+		path, _ := GetWorkingDir()
+
+		jsonConf := path + "/azion/azion.json"
+
+		os.MkdirAll(filepath.Dir(jsonConf), os.ModePerm)
+
+		azJsonData, err := GetAzionJsonContent()
+
+		require.NoError(t, err)
+		require.Contains(t, azJsonData.Name, "Test01")
+		require.Contains(t, azJsonData.Function.Name, "MyFunc")
+		require.Contains(t, azJsonData.Function.File, "myfile.js")
+		require.Contains(t, azJsonData.Function.Language, "javascript")
+		require.EqualValues(t, azJsonData.Function.Id, 476)
+	})
+}
