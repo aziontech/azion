f5b06439c9253436fdf29dbea4b2547e4f775ddf
Patrick Menoti <patrick.menoti@azion.com>
2022-08-19 10:12:44 -0300

    chore: change API Call timeout to 30 seconds

--- pkg/api/domains/domains.go
+++ pkg/api/domains/domains.go
@@ -39,7 +39,7 @@ func NewClient(c *http.Client, url string, token string) *Client {
 	conf.Servers = sdk.ServerConfigurations{
 		{URL: url},
 	}
-	conf.HTTPClient.Timeout = 10 * time.Second
+	conf.HTTPClient.Timeout = 30 * time.Second
 
 	return &Client{
 		apiClient: sdk.NewAPIClient(conf),
