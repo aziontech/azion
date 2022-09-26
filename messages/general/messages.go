3c584b7077c814844a4b9dacc914e40665be54a5
Patrick Menoti <patrick.menoti@azion.com>
2022-09-26 15:06:48 -0300

    chore: new help messages for azioncli and edge_functions

--- messages/general/messages.go
+++ messages/general/messages.go
@@ -2,10 +2,10 @@ package general
 
 var (
 	//List flags used by all azion apis
-	ApiListFlagDetails  = "Shows all relevant fields when listing"
-	ApiListFlagOrderBy  = "Identifies by which field the list should be sorted"
-	ApiListFlagSort     = "Defines which ordering will be used: <asc|desc>"
-	ApiListFlagPage     = "Identifies which page should be returned"
-	ApiListFlagPageSize = "Identifies how many items should be returned per page"
+	ApiListFlagDetails  = "Displays all relevant fields when listing"
+	ApiListFlagOrderBy  = "Sorts the output based on the selected field"
+	ApiListFlagSort     = "Defines the order of the items on the list; options <asc|desc>"
+	ApiListFlagPage     = "Returns a page of the list according to its number"
+	ApiListFlagPageSize = "Defines how many items should be returned per page"
 	ApiListFlagFilter   = "Filters items by their name"
 )
