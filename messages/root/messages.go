eb1e8a853f6cf46ea0eddab7c54cdf5310db447d
Patrick Menoti <patrick.menoti@azion.com>
2022-09-29 10:28:18 -0300

    fix: help messages

--- messages/root/messages.go
+++ messages/root/messages.go
@@ -1,7 +1,10 @@
 package root
 
 var (
-	RootUsage            = "azioncli"
+	RootUsage = `azioncli <command> <subcommand> [flags]
+azioncli [flags]
+ 	`
 	RootShortDescription = "Azion-CLI"
 	RootLongDescription  = "Calls the CLI via subcommands to interact with Azion services easily"
+	RootHelpFlag         = "Displays more information about the Azion CLI"
 )
