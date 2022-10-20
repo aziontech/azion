46be60a1342f4a629dd62d8543938b70ad80a25d
srmaxmazur <maxwel.mazur@azion.com>
2022-10-20 15:10:26 -0300

    ref: changing to be only in point of execution and not visible

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -1,1 +1,35 @@
-package completion
+package main
+
+import (
+	cmd "github.com/aziontech/azion-cli/pkg/cmd/root"
+	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/aziontech/azion-cli/pkg/iostreams"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var args string = "bash"
+
+func init() {
+	if len(os.Args) > 1 {
+		args = os.Args[1]
+	}
+}
+
+func main() {
+	cmd := cmd.NewRootCmd(&cmdutil.Factory{IOStreams: iostreams.System()})
+	GenerateAutocomplete(cmd, args)
+}
+
+func GenerateAutocomplete(rootCmd *cobra.Command, args string) {
+	switch args {
+	case "bash":
+		rootCmd.Root().GenBashCompletion(os.Stdout)
+	case "zsh":
+		rootCmd.Root().GenZshCompletion(os.Stdout)
+	case "fish":
+		rootCmd.Root().GenFishCompletion(os.Stdout, true)
+	case "powershell":
+		rootCmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+	}
+}
