4b19b155b32736cf1df3dd0d9d11b1393c2cb5af
srmaxmazur <maxwel.mazur@azion.com>
2022-10-20 15:28:55 -0300

    ref: add completion in cmd to test

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -4,5 +4,7 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run ../cmd/completion/completion.go "$sh" >"completions/azioncli.$sh"
+  go run ../cmd/azioncli/main.go completion "$sh" >"completions/azioncli.$sh"
+#	go run ../cmd/completion/completion.go "$sh" >"completions/azioncli.$sh"
 done
+#source completion/*
