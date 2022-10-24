640205cc6482fb40c8da9199fd23e3ee285fbca0
srmaxmazur <maxwel.mazur@azion.com>
2022-10-24 12:00:16 -0300

    ref: remove unused var

--- pkg/cmd/root/help.go
+++ pkg/cmd/root/help.go
@@ -51,10 +51,9 @@ func rootHelpFunc(f *cmdutil.Factory, command *cobra.Command, args []string) {
 	}
 
 	var (
-		baseCommands    []string
-		subcmdCommands  []string
-		examples        []string
-		skippedCommands []string
+		baseCommands   []string
+		subcmdCommands []string
+		examples       []string
 	)
 
 	for _, c := range command.Commands() {
@@ -68,7 +67,7 @@ func rootHelpFunc(f *cmdutil.Factory, command *cobra.Command, args []string) {
 		s := rpad(c.Name(), c.NamePadding()) + c.Short
 
 		if c.Annotations["Category"] == "skip" {
-			skippedCommands = append(skippedCommands, s)
+			continue
 		} else if !isRootCmd(c.Parent()) {
 			// Help of subcommand
 			subcmdCommands = append(subcmdCommands, s)
