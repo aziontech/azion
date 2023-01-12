e66017f8890daa180bcbd49898217975b72ca012
golpher96 <maxwelbm@hotmail.com>
2023-01-12 17:34:55 -0300

    ref: Improving the code with the number of pages, to use the returned error

--- pkg/cmd/edge_functions/list/list.go
+++ pkg/cmd/edge_functions/list/list.go
@@ -3,7 +3,7 @@ package list
 import (
 	"context"
 	"fmt"
-    "strings"
+	"strings"
 
 	"github.com/MakeNowJust/heredoc"
 	msg "github.com/aziontech/azion-cli/messages/edge_functions"
@@ -13,8 +13,8 @@ import (
 	"github.com/aziontech/azion-cli/pkg/printer"
 	"github.com/spf13/cobra"
 
-    "github.com/fatih/color"
-    table "github.com/MaxwelMazur/tablecli"
+	table "github.com/MaxwelMazur/tablecli"
+	"github.com/fatih/color"
 )
 
 func NewCmd(f *cmdutil.Factory) *cobra.Command {
@@ -34,73 +34,77 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		$ azioncli edge_functions list --sort "asc" 
 		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-            var numberPage int64 = opts.Page
+			var numberPage int64 = opts.Page
+			if !InsertTheFlagPage(cmd) {
+				for {
+					pages, err := PrintTable(f, opts, &numberPage)
+					if numberPage > pages {
+						return nil
+					}
+					if err != nil {
+						return fmt.Errorf(msg.ErrorGetFunctions.Error(), err)
+					}
+				}
+			}
 
-            if opts.Page == 1 {
-                for {
-                    if err := PrintTable(f, opts, &numberPage); err != nil {
-                        return nil
-                    }
-                }
-            }
-
-            if err := PrintTable(f, opts, &numberPage); err != nil {
+			if _, err := PrintTable(f, opts, &numberPage); err != nil {
 				return fmt.Errorf(msg.ErrorGetFunctions.Error(), err)
-            }
-
+			}
 			return nil
 		},
 	}
 
 	cmdutil.AddAzionApiFlags(cmd, opts)
 	cmd.Flags().BoolP("help", "h", false, msg.EdgeFunctionListHelpFlag)
-
 	return cmd
 }
 
-func PrintTable(f *cmdutil.Factory, opts *contracts.ListOptions, numberPage *int64) error { 
-	client := api.NewClient(f.HttpClient, f.Config.GetString("api_url"), f.Config.GetString("token"))
-    ctx := context.Background()
+func InsertTheFlagPage(cmd *cobra.Command) bool {
+	return cmd.Flags().Changed("page")
+}
 
-    table.DefaultWriter = f.IOStreams.Out
-    tbl := table.New("ID", "NAME", "LANGUAGE", "ACTIVE")
+func PrintTable(f *cmdutil.Factory, opts *contracts.ListOptions, numberPage *int64) (int64, error) {
+	client := api.NewClient(f.HttpClient, f.Config.GetString("api_url"), f.Config.GetString("token"))
+	ctx := context.Background()
+
+	table.DefaultWriter = f.IOStreams.Out
+	tbl := table.New("ID", "NAME", "LANGUAGE", "ACTIVE")
 	fields := []string{"GetId()", "GetName()", "GetLanguage()", "GetActive()"}
 
-	functions, err := client.List(ctx, opts)
+	functions, pages, err := client.List(ctx, opts)
 	if err != nil {
-		return fmt.Errorf(msg.ErrorGetFunctions.Error(), err)
+		return 0, fmt.Errorf(msg.ErrorGetFunctions.Error(), err)
 	}
 
 	if opts.Details {
 		fields = append(fields, "GetLastEditor()", "GetModified()", "GetReferenceCount()", "GetInitiatorType()")
-        tbl = table.New("ID", "NAME", "LANGUAGE", "ACTIVE", "LAST EDITOR", "MODIFIED", "REFERENCE COUNT", "INITIATOR_TYPE")
+		tbl = table.New("ID", "NAME", "LANGUAGE", "ACTIVE", "LAST EDITOR", "MODIFIED", "REFERENCE COUNT", "INITIATOR_TYPE")
 	}
 
-    headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
-    columnFmt := color.New(color.FgGreen).SprintfFunc()
-    tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgGreen).SprintfFunc()
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 
-	rows := printer.BuildRows(functions, fields) 
+	rows := printer.BuildRows(functions, fields)
 	for _, row := range rows {
-        if len(row) == 8 {
-            tbl.AddRow(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-        } else {
-            tbl.AddRow(row[0], row[1], row[2], row[3])
-        }
+		if len(row) == 8 {
+			tbl.AddRow(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+		} else {
+			tbl.AddRow(row[0], row[1], row[2], row[3])
+		}
 	}
 
 	format := strings.Repeat("%s", len(tbl.GetHeader())) + "\n"
-	tbl.CalculateWidths([]string{}) 
-    if *numberPage == 1 {
-        tbl.PrintHeader(format)
-    }
-
-	for _, row := range tbl.GetRows() {
-	    tbl.PrintRow(format, row)
+	tbl.CalculateWidths([]string{})
+	if *numberPage == 1 {
+		tbl.PrintHeader(format)
 	}
 
-    *numberPage +=1
-    opts.Page = *numberPage
-    return nil
-}
+	for _, row := range tbl.GetRows() {
+		tbl.PrintRow(format, row)
+	}
 
+	*numberPage += 1
+	opts.Page = *numberPage
+	return pages, nil
+}
