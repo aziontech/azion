c548a34aee68eae204112cf6d8d382422f933d6d
PatrickMenoti <patrick.menoti@azion.com>
2023-01-24 14:08:23 -0300

    feat: add delete command to domains

--- messages/domains/errors.go
+++ messages/domains/errors.go
@@ -4,4 +4,7 @@ import "errors"
 
 var (
 	ErrorGetDomains = errors.New("Failed to get the Domains: %s. Check your settings and try again. If the error persists, contact Azion support")
+
+	ErrorMissingDomainIdArgumentDelete = errors.New("A mandatory flag is missing. You must provide a domain_id as an argument. Run the command 'azioncli domains <subcommand> --help' to display more information and try again")
+	ErrorFailToDeleteDomain            = errors.New("Failed to delete the Domain: %s. Check your settings and try again. If the error persists, contact Azion support")
 )
