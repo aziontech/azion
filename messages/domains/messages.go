90e511f5d8db40182dcb91433a599cb81257720c
MaxwelMazur <maxwelbm@hotmail.com>
2023-01-25 15:59:16 -0300

    feat: adding new info messages regarding the domain

--- messages/domains/messages.go
+++ messages/domains/messages.go
@@ -12,4 +12,18 @@ var (
 	DomainsListShortDescription = "Displays yours domains"
 	DomainsListLongDescription  = "Displays all your domain references to your edges"
 	DomainsListHelpFlag         = "Displays more information about the list subcommand"
+
+	//create cmd
+	DomainsCreateUsage                    = "create [flags]"
+	DomainsCreateShortDescription         = "create new domain"
+	DomainsCreateLongDescription          = "creates a new domain to be used in edges_applications"
+	DomainsCreateFlagName                 = "The Domain name"
+	DomainsCreateFlagCnames               = "multiples domains name"
+	DomainsCreateFlagCnameAccessOnly      = "only access to the cnames"
+	DomainsCreateFlagDigitalCertificateId = "field where to put the id of the digital certificate"
+	DomainsCreateFlagEdgeApplicationId    = "edge applications reference field"
+	DomainsCreateFlagIsActive             = "boolean field to check if it is active"
+	DomainsCreateFlagIn                   = "Given file path to create an Domain; you can use - for reading from stdin"
+	DomainsCreateOutputSuccess            = "Created domain with ID %d\n"
+	DomainsCreateHelpFlag                 = "Displays more information about the create subcommand"
 )
