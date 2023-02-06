c821e824c9f7edd3e2b0bd47cf9f2da0090eea44
MaxwelMazur <maxwelbm@hotmail.com>
2023-02-06 13:09:34 -0300

    feat: add messages reference on command origins

---
+++ messages/origins/errors.go
@@ -0,0 +1,7 @@
+package origins
+
+import "errors"
+
+var (
+	ErrorGetOrigins = errors.New("Failed to describe the origins. Check your settings and try again. If the error persists, contact Azion support.")
+)
