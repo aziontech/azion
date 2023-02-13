8615978aed3d78bb6bcc6e77e030a225baf69589
gabriel-azion <gabriel.franca@azion.com>
2023-02-13 15:32:20 -0300

    docs: review messages and errors related to the origins command

--- messages/origins/errors.go
+++ messages/origins/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrorGetOrigins                   = errors.New("Failed to list your origins. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorGetOrigin                    = errors.New("Failed to retrieve the origin's data. Check your settings and try again. If the error persists, contact Azion support.")
-	ErrorMissingApplicationIDArgument = errors.New("A required flag is missing. You must supply an application_id as an argument. Run 'azioncli origins list --help' command to display more information and try again")
+	ErrorMissingApplicationIDArgument = errors.New("A required flag is missing. You must supply an 'application-id' as argument. Run 'azioncli origins list --help' command to display more information and try again")
 	ErrorMissingArguments             = errors.New("One or more required flags are missing. Run 'azioncli <command> <subcommand> --help' to display more information and try again")
 )
