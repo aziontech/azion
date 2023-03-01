e26c755427e8e2e25f0ccc599f90c1bfecea1bec
MaxwelMazur <maxwelbm@hotmail.com>
2023-03-01 10:05:14 -0300

    feat: command deleted created and test unit

--- pkg/cmd/cache_settings/cache_settings.go
+++ pkg/cmd/cache_settings/cache_settings.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aziontech/azion-cli/pkg/cmd/cache_settings/create"
 	"github.com/aziontech/azion-cli/pkg/cmd/cache_settings/list"
 	"github.com/aziontech/azion-cli/pkg/cmd/cache_settings/describe"
+	"github.com/aziontech/azion-cli/pkg/cmd/cache_settings/delete"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -27,6 +28,7 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 	cacheCmd.AddCommand(create.NewCmd(f))
 	cacheCmd.AddCommand(list.NewCmd(f))
 	cacheCmd.AddCommand(describe.NewCmd(f))
+	cacheCmd.AddCommand(delete.NewCmd(f))
 	cacheCmd.Flags().BoolP("help", "h", false, msg.CacheSettingsFlagHelp)
 	return cacheCmd
 }
---
+++ pkg/cmd/cache_settings/delete/delete.go
@@ -0,0 +1,48 @@
+package delete
+
+import (
+    "context"
+    "fmt"
+
+    "github.com/MakeNowJust/heredoc"
+    msg "github.com/aziontech/azion-cli/messages/cache_settings"
+    api "github.com/aziontech/azion-cli/pkg/api/edge_applications"
+    "github.com/aziontech/azion-cli/pkg/cmdutil"
+    "github.com/spf13/cobra"
+)
+
+var (
+    applicationID   int64
+    cacheSettingsID int64
+)
+
+func NewCmd(f *cmdutil.Factory) *cobra.Command {
+    cmd := &cobra.Command{
+        Use:           msg.CacheSettingsDeleteUsage,
+        Short:         msg.CacheSettingsDeleteShortDescription,
+        Long:          msg.CacheSettingsDeleteLongDescription,
+        SilenceUsage:  true,
+        SilenceErrors: true,
+        Example: heredoc.Doc(`
+        $ azioncli cache delete --application-id 1234 --cache-settings-id 03a6e7bf-8e26-49c7-a66e-ab8eaa425086
+        $ azioncli cache delete -a 1234 -c 03a6e7bf-8e26-49c7-a66e-ab8eaa425086
+        `),
+        RunE: func(cmd *cobra.Command, args []string) error {
+            if !cmd.Flags().Changed("application-id") || !cmd.Flags().Changed("cache-settings-id") {
+                return msg.ErrorMissingArguments
+            }
+            if err := api.NewClient(f.HttpClient, f.Config.GetString("api_url"), f.Config.GetString("token")).
+            DeleteCacheSettings(context.Background(), applicationID, cacheSettingsID); err != nil {
+                return fmt.Errorf(msg.ErrorFailToDelete.Error(), err)
+            }
+            fmt.Fprintf(f.IOStreams.Out, msg.CacheSettingsDeleteOutputSuccess, cacheSettingsID)
+            return nil
+        },
+    }
+
+
+    cmd.Flags().Int64VarP(&applicationID, "application-id", "a", 0, msg.CacheSettingsDeleteFlagApplicationID)
+    cmd.Flags().Int64VarP(&cacheSettingsID, "cache-settings-id", "c", 0, msg.CacheSettingsDeleteFlagCacheSettingsID)
+    cmd.Flags().BoolP("help", "h", false, msg.CacheSettingsDeleteHelpFlag)
+    return cmd
+}
