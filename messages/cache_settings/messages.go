3be5a80b35948642203af3d04c4760813c937566
gabriel-azion <gabriel.franca@azion.com>
2023-03-02 13:55:28 -0300

    cache settings messages

--- messages/cache_settings/messages.go
+++ messages/cache_settings/messages.go
@@ -3,49 +3,49 @@ package cache_settings
 var (
     // [ cache_settings ]
     CacheSettingsUsage            = "cache_settings"
-    CacheSettingsShortDescription = "Cache Settings lets you check, remove or update existing settings, besides creating new ones"
-    CacheSettingsLongDescription  = "Cache Settings lets you check, remove or update existing settings, besides creating new ones"
+    CacheSettingsShortDescription = "Cache Settings allows you to check, remove or update existing settings and create new ones"
+    CacheSettingsLongDescription  = "Cache Settings allows you to check, remove or update existing settings and create new ones"
     CacheSettingsFlagHelp         = "Displays more information about the cache_settings command"
-    CacheSettingsId               = "Unique identifier for a cache setting"
+    CacheSettingsId               = "Unique identifier for a Cache Settings configuration"
 
     // [ list ]
     CacheSettingsListUsage            = "list [flags]"
-    CacheSettingsListShortDescription = "Displays yours cache settings"
+    CacheSettingsListShortDescription = "Displays your cache settings"
     CacheSettingsListLongDescription  = "Displays all cache settings"
     CacheSettingsListHelpFlag         = "Displays more information about the list subcommand"
 
     // [ create ]
     CacheSettingsCreateUsage                          = "create [flags]"
     CacheSettingsCreateShortDescription               = "Creates a new Cache Setting"
-    CacheSettingsCreateLongDescription                = "Creates a Cache Setting based on given attributes to be used in edge applications"
+    CacheSettingsCreateLongDescription                = "Creates a Cache Settings configuration based on given attributes to be used in edge applications"
     CacheSettingsCreateFlagEdgeApplicationId          = "Unique identifier for an edge application"
     CacheSettingsCreateFlagName                       = "The Cache Settings' name"
     CacheSettingsCreateFlagIn                         = "Path to a JSON file containing the attributes of the Cache Setting that will be created; you can use - for reading from stdin"
     CacheSettingsCreateOutputSuccess                  = "Created cache setting with ID %d\n"
     CacheSettingsCreateHelpFlag                       = "Displays more information about the create subcommand"
-    CacheSettingsCreateFlagBrowserCacheSettings       = "Browser Cache Settings"
+    CacheSettingsCreateFlagBrowserCacheSettings       = "Browser Cache Settings" // Check
     CacheSettingsCreateFlagQueryStringFields          = "Cache Settings' query string fields"
-    CacheSettingsCreateFlagCookieNames                = "Cache Settings' cookie names"
-    CacheSettingsCreateFlagCacheByCookies             = "Whether cache by cookies is active or not"
-    CacheSettingsCreateFlagCacheByQueryString         = "Cache Settings' cache by query string"
-    CacheSettingsCreateFlagCdnCacheSettings           = "CDN cache settings"
+    CacheSettingsCreateFlagCookieNames                = "Cache Settings' cookie names" // Check
+    CacheSettingsCreateFlagCacheByCookies             = "Whether cache by cookies is active or not" // Check
+    CacheSettingsCreateFlagCacheByQueryString         = "Cache Settings' cache by query string" // Check
+    CacheSettingsCreateFlagCdnCacheSettings           = "CDN cache settings" // Check
     CacheSettingsCreateFlagCachingForOptions          = "Whether caching for options is active or not"
     CacheSettingsCreateFlagCachingStringSort          = "Whether caching string sort is active or not"
     CacheSettingsCreateFlagCachingForPost             = "Whether caching for post is active or not"
     CacheSettingsCreateFlagSliceConfigurationEnabled  = "Whether slice configuration is active or not"
     CacheSettingsCreateFlagSliceL2CachingEnabled      = "Whether slice L2 caching is active or not"
     CacheSettingsCreateFlagSliceEdgeCachingEnabled    = "Whether slice edge caching is active or not"
-    CacheSettingsCreateFlagL2CachingEnabled           = "Whether slice edge caching is active or not"
-    CacheSettingsCreateFlagSliceConfigurationRange    = "Cache Settings' slice configuration range"
-    CacheSettingsCreateFlagCdnCacheSettingsMaxTtl     = "CDN cache settings' maximum TTL"
-    CacheSettingsCreateFlagBrowserCacheSettingsMaxTtl = "Browser cache settings' maximum TTL"
-    CacheSettingsCreateFlagAdaptiveDeliveryAction     = "Cache Settings' adaptive delivery action"
+    CacheSettingsCreateFlagL2CachingEnabled           = "Whether slice L2 caching is active or not"
+    CacheSettingsCreateFlagSliceConfigurationRange    = "Cache Settings' slice configuration range" // Check
+    CacheSettingsCreateFlagCdnCacheSettingsMaxTtl     = "CDN cache settings' maximum TTL" // Check
+    CacheSettingsCreateFlagBrowserCacheSettingsMaxTtl = "Browser cache settings' maximum TTL" // Check
+    CacheSettingsCreateFlagAdaptiveDeliveryAction     = "Cache Settings' adaptive delivery action" // Check
 
     // [ update ]
     CacheSettingsUpdateUsage            = "update [flags]"
-    CacheSettingsUpdateShortDescription = "Modifies a Cache Setting"
-    CacheSettingsUpdateLongDescription  = "Modifies a Cache Setting based on given attributes to be used in edge applications"
-    CacheSettingsUpdateOutputSuccess    = "Updated cache setting with ID %d\n"
+    CacheSettingsUpdateShortDescription = "Updates a Cache Settings configuration"
+    CacheSettingsUpdateLongDescription  = "Updates a Cache Settings configuration based on given attributes to be used in edge applications"
+    CacheSettingsUpdateOutputSuccess    = "Updated a Cache Settings configuration with ID %d\n"
 
     // [ describe ]
     CacheSettingsDescribeUsage               = "describe --application-id <application_id> --cache-settings-id <cache-settings-id> [flags]"
@@ -59,8 +59,8 @@ var (
 
     // [ delete ]
     CacheSettingsDeleteUsage               = "delete [flags]"
-    CacheSettingsDeleteShortDescription    = "Deletes an Cache Settings"
-    CacheSettingsDeleteLongDescription     = "Deletes an Caches Settings from the Edge Applications library based on its given ID"
+    CacheSettingsDeleteShortDescription    = "Deletes a Cache Settings"
+    CacheSettingsDeleteLongDescription     = "Deletes a Caches Settings from the Edge Applications library based on its given ID"
     CacheSettingsDeleteOutputSuccess       = "Caches settings %d was successfully deleted\n"
     CacheSettingsDeleteFlagApplicationID   = "Unique identifier for an edge application"
     CacheSettingsDeleteFlagCacheSettingsID = "The Cache Settings key unique identifier"
