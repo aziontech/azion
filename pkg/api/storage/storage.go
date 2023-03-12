876d83fa9905a082cc8791aa538bf25f11b02cf2
PatrickMenoti <patrick.menoti@azion.com>
2023-03-12 17:28:21 -0300

    fix: build project before reading azion file

--- pkg/api/storage/storage.go
+++ pkg/api/storage/storage.go
@@ -14,29 +14,29 @@ import (
 )
 
 type Client struct {
-    apiClient *sdk.APIClient
+	apiClient *sdk.APIClient
 }
 
 func NewClient(c *http.Client, url string, token string) *Client {
-    conf := sdk.NewConfiguration()
-    conf.AddDefaultHeader("Authorization", "Token "+token)
-    conf.AddDefaultHeader("Accept", "application/json; version=3")
-    conf.AddDefaultHeader("Content-Type", "application/json")
-    conf.UserAgent = "Azion_CLI/" + version.BinVersion
-    conf.Servers = sdk.ServerConfigurations{
-        {URL: url},
-    } 
-    return &Client{
-        apiClient: sdk.NewAPIClient(conf),
-    }
+	conf := sdk.NewConfiguration()
+	conf.AddDefaultHeader("Authorization", "Token "+token)
+	conf.AddDefaultHeader("Accept", "application/json; version=3")
+	conf.AddDefaultHeader("Content-Type", "application/json")
+	conf.UserAgent = "Azion_CLI/" + version.BinVersion
+	conf.Servers = sdk.ServerConfigurations{
+		{URL: url},
+	}
+	return &Client{
+		apiClient: sdk.NewAPIClient(conf),
+	}
 }
 
-func (c *Client) Upload(ctx context.Context, versionID, path string, file *os.File) (error) { 
-    c.apiClient.GetConfig().DefaultHeader["Content-Disposition"] = fmt.Sprintf("attachment; filename=\"%s\"", path)
-    req := c.apiClient.DefaultApi.StorageVersionIdPost(ctx, versionID).XAzionStaticPath(path).Body(file)
-    _, httpResp, err := req.Execute()
-    if err != nil {
-        return utils.ErrorPerStatusCode(httpResp, err)
-    }
-    return nil
+func (c *Client) Upload(ctx context.Context, versionID, path string, file *os.File) error {
+	c.apiClient.GetConfig().DefaultHeader["Content-Disposition"] = fmt.Sprintf("attachment; filename=\"%s\"", path)
+	req := c.apiClient.DefaultApi.StorageVersionIdPost(ctx, versionID).XAzionStaticPath(path).Body(file)
+	_, httpResp, err := req.Execute()
+	if err != nil {
+		return utils.ErrorPerStatusCode(httpResp, err)
+	}
+	return nil
 }
