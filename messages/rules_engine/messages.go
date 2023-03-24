7443ceac4fee7bddfaec5cdd6508cc3bbd81e4a5
MaxwelMazur <maxwelbm@hotmail.com>
2023-03-24 10:06:59 -0300

    feat: new command delete from rules engine

--- messages/rules_engine/messages.go
+++ messages/rules_engine/messages.go
@@ -13,4 +13,14 @@ var (
 	RulesEngineListLongDescription  = "Displays all your rules engine references to your edge locations"
 	RulesEngineListHelpFlag         = "Displays more information about the list subcommand"
 	RulesEngineListHelpPhase        = "Rules Engine Phase <request|response>"
+
+	// [ delete ]
+	RulesEngineDeleteUsage             = "delete [flags]"
+	RulesEngineDeleteShortDescription  = "Deletes an Rules Engine"
+	RulesEngineDeleteLongDescription   = "Deletes an Rules Engine from the Edge Applications library based on its given ID"
+	RulesEngineDeleteOutputSuccess     = "Rules Engine %d was successfully deleted\n"
+	RulesEngineDeleteFlagApplicationID = "Unique identifier for an edge application"
+	RulesEngineDeleteFlagRuleID        = "The Rules Engine 's key unique identifier"
+	RulesEngineDeleteFlagPhase         = "phase is request input or response output"
+	RulesEngineDeleteHelpFlag          = "Displays more information about the delete subcommand"
 )
