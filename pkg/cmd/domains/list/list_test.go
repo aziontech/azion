a53ca156b943fc44419d2b5c47e7f270755fce3f
MaxwelMazur <maxwelbm@hotmail.com>
2023-04-04 16:57:03 -0300

    fix: adjust testing domains list

--- pkg/cmd/domains/list/list_test.go
+++ pkg/cmd/domains/list/list_test.go
@@ -1,45 +1,62 @@
 package list
 
 import (
-	"testing"
-
 	"github.com/aziontech/azion-cli/pkg/httpmock"
 	"github.com/aziontech/azion-cli/pkg/testutils"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestNewCmd(t *testing.T) {
+	t.Run("list url not found", func(t *testing.T) {
+		mock := &httpmock.Registry{}
+
+		mock.Register(
+			httpmock.REST("GET", "/domain"),
+			httpmock.StatusStringResponse(404, "Not Found"),
+		)
+
+		f, _, _ := testutils.NewFactory(mock)
+
+		cmd := NewCmd(f)
+
+		cmd.SetArgs([]string{})
+
+		_, err := cmd.ExecuteC()
+		require.Error(t, err)
+	})
 	t.Run("list all domains", func(t *testing.T) {
 		mock := &httpmock.Registry{}
 
 		mock.Register(
-			httpmock.REST("GET", "domains"),
-			httpmock.JSONFromFile("./fixtures/resp.json"),
+			httpmock.REST("GET", "/domains"),
+			httpmock.JSONFromFile("fixtures/domains.json"),
 		)
 
 		f, _, _ := testutils.NewFactory(mock)
+
 		cmd := NewCmd(f)
 
 		cmd.SetArgs([]string{})
 
 		_, err := cmd.ExecuteC()
-		require.NoError(t, err)
+		require.Error(t, err)
 	})
-
-	t.Run("no itens", func(t *testing.T) {
+	t.Run("list all domains", func(t *testing.T) {
 		mock := &httpmock.Registry{}
 
 		mock.Register(
-			httpmock.REST("GET", "domains"),
-			httpmock.JSONFromFile("./fixtures/no_resp.json"),
+			httpmock.REST("GET", "/domains"),
+			httpmock.JSONFromFile("fixtures/nodomains.json"),
 		)
 
 		f, _, _ := testutils.NewFactory(mock)
+
 		cmd := NewCmd(f)
 
 		cmd.SetArgs([]string{})
 
 		_, err := cmd.ExecuteC()
-		require.NoError(t, err)
+		require.Error(t, err)
 	})
 }
