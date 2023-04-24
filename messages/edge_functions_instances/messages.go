1f8835ddda3b512083e59355035208799f78098b
gabriel-azion <gabriel.franca@azion.com>
2023-04-24 09:33:29 -0300

    feat: review and create messages

--- messages/edge_functions_instances/messages.go
+++ messages/edge_functions_instances/messages.go
@@ -3,51 +3,52 @@ package edge_functions_instances
 var (
 	// [ EdgeFunctionsInstances ]
 	EdgeFunctionsInstancesUsage            = "edge_functions_instances"
-	EdgeFunctionsInstancesShortDescription = "edge functions instances is the original source of data."
-	EdgeFunctionsInstancesLongDescription  = "EdgeFunctionsInstances is the original source of data on edge platforms, where data is fetched when cache is not available."
+	EdgeFunctionsInstancesShortDescription = "Edge Functions Instances allows you to instantiate serverless functions in your edge applications at Azion."
+	EdgeFunctionsInstancesLongDescription  = "Edge Functions Instances allows you to instantiate serverless functions in your edge applications at Azion, as well as set up conditions for their execution."
 	EdgeFunctionsInstancesFlagHelp         = "Displays more information about the edge functions instances command"
 
 	// [ list ]
 	EdgeFunctionsInstancesListUsage                 = "list [flags]"
-	EdgeFunctionsInstancesListShortDescription      = "Displays your edge functions instances"
-	EdgeFunctionsInstancesListLongDescription       = "Displays all edge functions instances related to your applications"
+	EdgeFunctionsInstancesListShortDescription      = "Displays your edge functions instances."
+	EdgeFunctionsInstancesListLongDescription       = "Displays all edge functions instances related to a specific edge application."
 	EdgeFunctionsInstancesListHelpFlag              = "Displays more information about the list subcommand"
 	EdgeFunctionsInstancesListFlagEdgeApplicationID = "Unique identifier for an edge application."
 
 	// [ flags ]
-	EdgeApplicationFlagId        = "Unique identifier of the Edge Application"
-	EdgeFunctionsInstancesFlagId = "Unique identifier of the Edge Functions Instances"
+	EdgeApplicationFlagId        = "Unique identifier for an edge application"
+	EdgeFunctionsInstancesFlagId = "Unique identifier for an edge functions instance"
 
 	//Edge Functions Instances cmd
 	EdgeFuncInstanceUsage            = "edge_functions_instances"
-	EdgeFuncInstanceShortDescription = "Create Edge Functions Instances for edges on Azion's platform"
-	EdgeFuncInstanceLongDescription  = "Create Edge Functions Instances for edges on Azion's platform"
-	EdgeFuncInstanceFlagHelp         = "Displays more information about the edge_functions_instances command"
-	EdgeFuncInstanceFlagId           = "Unique identifier of the Edge Function Instance"
-	ApplicationFlagId                = "Unique identifier for an edge application used by this domain.. The '--application-id' flag is mandatory"
+	EdgeFuncInstanceShortDescription = "Edge Functions Instances allows you to instantiate serverless functions in your edge applications at Azion."
+	EdgeFuncInstanceLongDescription  = "Edge Functions Instances allows you to instantiate serverless functions in your edge applications at Azion, as well as set up conditions for its execution."
+	EdgeFuncInstanceFlagHelp         = "Displays more information about the edge functions instances command"
+	EdgeFuncInstanceFlagId           = "Unique identifier for an edge functions instance"
+	ApplicationFlagId                = "Unique identifier for the edge application related to an edge functions instance. The '--application-id' flag is required"
 
 	//delete cmd
-	EdgeFuncInstanceDeleteUsage            = "delete --application-id <application_id> --function-id <function_id> [flags]"
-	EdgeFuncInstanceDeleteShortDescription = "Removes an Edge Function Instance"
-	EdgeFuncInstanceDeleteLongDescription  = "Removes an Edge Function Instance from the Domains library based on its given ID"
-	EdgeFuncInstanceDeleteOutputSuccess    = "Edge Function Instance %s was successfully deleted\n"
+	EdgeFuncInstanceDeleteUsage            = "delete --application-id <application_id> --instance-id <instance-id>"
+	EdgeFuncInstanceDeleteShortDescription = "Removes an edge functions instance"
+	EdgeFuncInstanceDeleteLongDescription  = "Removes an edge functions instance, instantiated in a specific edge application, based on the given flags."
+	EdgeFuncInstanceDeleteOutputSuccess    = "Edge functions instance %s was successfully deleted\n"
 	EdgeFuncInstanceDeleteHelpFlag         = "Displays more information about the delete subcommand"
 
 	// [ create ]
 	EdgeFuncInstanceCreateUsage                 = "create [flags]"
-	EdgeFuncInstanceCreateShortDescription      = "Creates a new Function Instances"
-	EdgeFuncInstanceCreateLongDescription       = "Creates an Function Instances based on given attributes to be used in edge applications"
+	EdgeFuncInstanceCreateShortDescription      = "Creates a new edge functions instance"
+	EdgeFuncInstanceCreateLongDescription       = "Creates a new edge functions instance based on given attributes to be used in an edge application"
 	EdgeFuncInstanceCreateFlagEdgeApplicationId = "Unique identifier for an edge application"
-	EdgeFuncInstanceCreateFlagEdgeFunctionID    = "Unique identifier for an Edge Function Instances"
-	EdgeFuncInstanceCreateFlagName              = "The Function Instances name"
-	EdgeFuncInstanceCreateFlagArgs              = "The args name"
-	EdgeFuncInstanceCreateFlagIn                = "Path to a JSON file containing the attributes of the origin that will be created; you can use - for reading from stdin"
+	EdgeFuncInstanceCreateFlagEdgeFunctionID    = "Unique identifier for an edge functions instance"
+	EdgeFuncInstanceCreateFlagName              = "The edge functions instance name"
+	EdgeFuncInstanceCreateFlagArgs              = "The JSON args related to the edge functions instance being created"
+	EdgeFuncInstanceCreateFlagIn                = "Path to a JSON file containing the attributes of the edge functions instance being created; you can use - for reading from stdin"
 	EdgeFuncInstanceCreateOutputSuccess         = "Created Function Instances with ID %d\n"
 	EdgeFuncInstanceCreateHelpFlag              = "Displays more information about the create subcommand"
+
 	//describe cmd
 	EdgeFuncInstanceDescribeUsage            = "describe --application-id <application_id> --instance-id <instance_id> [flags]"
-	EdgeFuncInstanceDescribeShortDescription = "Returns the information related to the Edge Function Instance"
-	EdgeFuncInstanceDescribeLongDescription  = "Returns the information related to the Edge Function Instance, informed through the flag '--instance-id' in detail"
+	EdgeFuncInstanceDescribeShortDescription = "Returns the information related to the edge functions instance"
+	EdgeFuncInstanceDescribeLongDescription  = "Returns the information related to the edge functions instance, informed through the flag '--instance-id' in detail"
 	EdgeFuncInstanceDescribeFlagOut          = "Exports the output of the subcommand 'describe' to the given file path <file_path/file_name.ext>"
 	EdgeFuncInstanceDescribeFlagFormat       = "Changes the output format passing the json value to the flag. Example '--format json'"
 	EdgeFuncInstanceDescribeHelpFlag         = "Displays more information about the describe subcommand"
@@ -56,14 +57,14 @@ var (
 
 	// [ Update ]
 	EdgeFuncInstanceUpdateUsage                 = "update --application-id <application_id> --instance-id <instance_id> [flags]"
-	EdgeFuncInstanceUpdateShortDescription      = "Updates an edge function instance"
-	EdgeFuncInstanceUpdateLongDescription       = "Updates an edge function instance based on given attributes to be used in edge applications"
+	EdgeFuncInstanceUpdateShortDescription      = "Updates an edge functions instance"
+	EdgeFuncInstanceUpdateLongDescription       = "Updates an edge functions instance, based on given attributes, to be used in edge applications"
 	EdgeFuncInstanceUpdateFlagEdgeApplicationId = "Unique identifier for an edge application"
-	EdgeFuncInstanceUpdateFlagIn                = "Path to a JSON file containing the attributes of the rule that will be updated; you can use - for reading from stdin"
-	EdgeFuncInstanceUpdateFlagName              = "Edge Function Instance name"
-	EdgeFuncInstanceUpdateFlagArgs              = "Edge Function Instance args"
-	EdgeFuncInstanceUpdateFlagInstanceID        = "Unique identifier for an edge function instance"
-	EdgeFuncInstanceUpdateFlagFunctionID        = "Edge Function ID to be used in the Edge Function Instance"
-	EdgeFuncInstanceUpdateOutputSuccess         = "Updated edge function instance with ID %d\n"
+	EdgeFuncInstanceUpdateFlagIn                = "Path to a JSON file containing the attributes of the edge functions instance that will be updated; you can use - for reading from stdin"
+	EdgeFuncInstanceUpdateFlagName              = "The edge functions instance name"
+	EdgeFuncInstanceUpdateFlagArgs              = "The JSON args related to the edge functions instance being created"
+	EdgeFuncInstanceUpdateFlagInstanceID        = "Unique identifier for an edge functions instance"
+	EdgeFuncInstanceUpdateFlagFunctionID        = "Edge function ID to be used in the edge functions instance"
+	EdgeFuncInstanceUpdateOutputSuccess         = "Updated edge functions instance with ID %d\n"
 	EdgeFuncInstanceUpdateHelpFlag              = "Displays more information about the update subcommand"
 )
