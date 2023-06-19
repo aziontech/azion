88ac9fc9c8bcf105607f58e676921803b9810f11
maxwelbm <maxwelbm@hotmail.com>
2023-06-19 11:44:49 -0300

    chore: change messages to be more understandable

--- messages/variables/messages.go
+++ messages/variables/messages.go
@@ -4,14 +4,14 @@ var (
 	// [ variables ]
 	Usage            = "variables"
 	ShortDescription = "Create variables for edges on Azion's platform"
-	LongDescription  = "Build your Web applications in minutes without the need to manage infrastructure or security"
+	LongDescription  = "Create variables for edges on Azion's platform"
 	FlagHelp         = "Displays more information about the variables command"
 	FlagId           = "Unique identifier of the Variable"
 
 	// [ describe ]
 	DescribeUsage            = "describe --variable-id <variable_id> [flags]"
 	DescribeShortDescription = "Returns the variable data"
-	DescribeLongDescription  = "Displays information about the variable via a given ID to show the application’s attributes in detail"
+	DescribeLongDescription  = "to show the variable's attributes in detail"
 	DescribeFlagVariableID   = "Unique identifier for an variable. The '--variable-id' flag is mandatory"
 	DescribeFlagOut          = "Exports the output to the given <file_path/file_name.ext>"
 	DescribeFlagFormat       = "Changes the output format passing the json value to the flag"
