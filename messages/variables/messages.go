4e39d67c8bce7a9c9ec74b7e6a8c5cceca32e860
maxwelbm <maxwelbm@hotmail.com>
2023-06-19 11:54:52 -0300

    chore: change messages to be more understandable

--- messages/variables/messages.go
+++ messages/variables/messages.go
@@ -11,7 +11,7 @@ var (
 	// [ describe ]
 	DescribeUsage            = "describe --variable-id <variable_id> [flags]"
 	DescribeShortDescription = "Returns the variable data"
-	DescribeLongDescription  = "to show the variable's attributes in detail"
+	DescribeLongDescription  = "Displays information about the variable via a given ID to show the variable's attributes in detail"
 	DescribeFlagVariableID   = "Unique identifier for an variable. The '--variable-id' flag is mandatory"
 	DescribeFlagOut          = "Exports the output to the given <file_path/file_name.ext>"
 	DescribeFlagFormat       = "Changes the output format passing the json value to the flag"
