5d2c65c3407c75a8a214bd1d8ebe9651fb834915
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-06-20 11:14:15 -0300

    refactor: change messages

--- messages/variables/errors.go
+++ messages/variables/errors.go
@@ -8,6 +8,6 @@ var (
 	ErrorGetVariables                    = errors.New("Failed to describe the variables: %s. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorFailToDeleteVariable            = errors.New("Failed to delete the Variable: %s. Check your settings and try again. If the error persists, contact Azion support")
 	ErrorMissingVariableIdArgumentDelete = errors.New("A mandatory flag is missing. You must provide a variable_id as an argument. Run the command 'azioncli variables <subcommand> --help' to display more information and try again")
-	ErrorMandatoryCreateFlags            = errors.New("One or more required flags are missing. You must provide --key, --name, and --secret flags when the --in flag is not provided. Run the command 'azioncli edge_applications create --help' to display more information and try again")
+	ErrorMandatoryCreateFlags            = errors.New("One or more required flags are missing. You must provide --key, --name, and --secret flags when the --in flag is not provided. Run the command 'azioncli variables create --help' to display more information and try again")
 	ErrorCreateItem                      = errors.New("Failed to create the variable: %s. Check your settings and try again. If the error persists, contact Azion support.")
 )
