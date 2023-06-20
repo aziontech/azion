4676013330b20a79abacd11a621d26571b3775d4
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-06-20 10:48:47 -0300

    refactor: a better suggestion

--- pkg/cmd/variables/create/create.go
+++ pkg/cmd/variables/create/create.go
@@ -32,7 +32,7 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		Example: heredoc.Doc(`
-        $ azioncli variables create --key "key" --value "value" --secret false
+	$ azioncli variables create --key "Content-Type" --value "string" --secret false
         $ azioncli variables create --in "create.json"
         `),
 		RunE: func(cmd *cobra.Command, args []string) error {
