2c3af29a50c17d0d3b54b360d633c27f52cbf96a
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-06-20 10:46:19 -0300

    fix: change origin to variable in messages

--- messages/variables/errors.go
+++ messages/variables/errors.go
@@ -3,7 +3,7 @@ package variables
 import "errors"
 
 var (
-	ErrorGetVariables                    = errors.New("Failed to describe the origins: %s. Check your settings and try again. If the error persists, contact Azion support.")
+	ErrorGetVariables                    = errors.New("Failed to describe the variable: %s. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorGetItem                         = errors.New("Failed to describe the variable: %s. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorMissingArguments                = errors.New("Required flags are missing. You must supply application-id and variable-id as arguments. Run 'azioncli <command> <subcommand> --help' command to display more information and try again")
 	ErrorFailToDeleteVariable            = errors.New("Failed to delete the Variable: %s. Check your settings and try again. If the error persists, contact Azion support")
