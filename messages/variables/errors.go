f41d1ed1d03ed7282670a5a72a1fb50d7ad8692c
gabriel-azion <gabriel.franca@azion.com>
2023-06-22 11:38:29 -0300

    review errors messsages

--- messages/variables/errors.go
+++ messages/variables/errors.go
@@ -6,7 +6,7 @@ var (
 	ErrorGetItem                         = errors.New("Failed to describe the variable: %s. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorMissingArguments                = errors.New("A required flag is missing. You must supply the --variable-id flag as an argument. Run 'azioncli <command> <subcommand> --help' command to display more information and try again")
 	ErrorFailToDeleteVariable            = errors.New("Failed to delete the variable: %s. Check your settings and try again. If the error persists, contact Azion support")
-	ErrorMissingVariableIdArgumentDelete = errors.New("A mandatory flag is missing. You must provide the --variable_id flag as an argument. Run the command 'azioncli variables <subcommand> --help' to display more information and try again")
+	ErrorMissingVariableIdArgumentDelete = errors.New("A required flag is missing. You must provide the --variable_id flag as an argument. Run the command 'azioncli variables <subcommand> --help' to display more information and try again")
 	ErrorMissingVariableIdArgument       = errors.New("Required flags are missing. You must provide the --variable-id, --key, --value, and --secret flags as arguments, or the --in flag informing the path to import the file. Run the command 'azioncli variables <subcommand> --help' to display more information and try again")
 	ErrorSecretFlag                      = errors.New("Invalid --secret flag provided. The value must be 'true' or 'false'. Run the command 'azioncli variables <subcommand> --help' to display more information and try again")
 	ErrorUpdateVariable                  = errors.New("Failed to update the variable: %s. Check your settings and try again. If the error persists, contact Azion support")
