47b07f06a28ff1e9f4634dfd312a696f60b3674c
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-07-17 11:06:59 -0300

    chore: message
    Co-authored-by: Gabriel França <119522814+gabriel-azion@users.noreply.github.com>

--- utils/errors.go
+++ utils/errors.go
@@ -41,7 +41,7 @@ var (
 	ErrorTimeoutAPICall         = errors.New("CLI's request has timed out during communication with Azion. Verify if it has completed successfully or wait some time and try the command again")
 	ErrorCreateFile             = errors.New("Failed to create %s file")
 	ErrorProductNotOwned        = errors.New("This account does not own the following product")
-	ErrorUnknownSystem          = errors.New("unknown system")
+	ErrorUnknownSystem          = errors.New("Unknown system")
 	ErrorCommandNotFound        = errors.New("Command '%s' not found")
 	ErrorGetAssetsNamesAzioncli = errors.New("failed to fetch the assets names from azioncli")
 )
