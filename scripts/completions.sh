be5d7950e057c7853ae86b1945a49a6211fb38f6
maxwelbm <maxwelbm@hotmail.com>
2023-07-21 10:56:47 -0300

    chore: added flag not update in generated auto completion

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 # scripts/completions.sh
-set -e
+
 rm -rf completions
 mkdir completions
-for sh in bash zsh fish; do
-  go run cmd/azioncli/main.go completion "$sh" >"completions/azioncli.$sh"
+
+generate_completions() {
+  local shell=$1
+  local output_file="completions/azioncli.$shell"
+
+  if go run cmd/azioncli/main.go completion "$shell" --no-update > "$output_file"; then
+    echo "Completions generated for $shell"
+  else
+    echo "Failed to generate completions for $shell"
+  fi
+}
+
+shells=("bash" "zsh" "fish")
+
+for shell in "${shells[@]}"; do
+  generate_completions "$shell"
 done
