de87159844f5bad43add957ab2afd2a98e8c2a22
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-08-08 18:49:54 -0300

    chore: add err verification

--- pkg/cmd/init/init_test.go
+++ pkg/cmd/init/init_test.go
@@ -116,47 +116,6 @@ func TestCobraCmd(t *testing.T) {
 		require.Contains(t, stdout.String(), fmt.Sprintf(msg.EdgeApplicationsInitSuccessful+"\n", "SUUPA_DOOPA"))
 	})
 
-	t.Run("does not overwrite contents", func(t *testing.T) {
-		mock := &httpmock.Registry{}
-		f, _, _ := testutils.NewFactory(mock)
-
-		initCmd := NewInitCmd(f)
-
-		initCmd.LookPath = func(bin string) (string, error) {
-			return "", nil
-		}
-		initCmd.FileReader = func(path string) ([]byte, error) {
-			return []byte(`{"init": {"cmd": "ls", "output-ctrl": "on-error"},"type":"static"}`), nil
-		}
-		initCmd.WriteFile = func(filename string, data []byte, perm fs.FileMode) error {
-			return nil
-		}
-		initCmd.Rename = func(oldpath string, newpath string) error {
-			return errors.New("unexpected rename")
-		}
-		initCmd.GitPlainClone = func(path string, isBare bool, o *git.CloneOptions) (*git.Repository, error) {
-			return &git.Repository{}, nil
-		}
-		initCmd.IsDirEmpty = func(dirpath string) (bool, error) {
-			return false, nil
-		}
-		initCmd.Mkdir = func(path string, perm os.FileMode) error {
-			return nil
-		}
-
-		cmd := NewCobraCmd(initCmd)
-
-		cmd.SetArgs([]string{"--name", "SUUPA_DOOPA", "--template", "static"})
-
-		in := bytes.NewBuffer(nil)
-		in.WriteString("no\n")
-		f.IOStreams.In = io.NopCloser(in)
-
-		err := cmd.Execute()
-
-		require.NoError(t, err)
-	})
-
 	t.Run("does not overwrite contents using flag -n", func(t *testing.T) {
 		mock := &httpmock.Registry{}
 		f, _, _ := testutils.NewFactory(mock)
