762ef503f4e0ad770f2877dacb7e7f96ac4ffe5c
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-08-11 11:23:01 -0300

    fix: tests were failing

--- pkg/cmd/init/init.go
+++ pkg/cmd/init/init.go
@@ -121,18 +121,18 @@ func (cmd *InitCmd) run(info *InitInfo, options *contracts.AzionApplicationOptio
 	}
 	info.PathWorkingDir = path
 
-	if !c.Flags().Changed("template") || !c.Flags().Changed("mode") {
-		err = cmd.selectVulcanTemplates(info)
-		if err != nil {
-			return err
-		}
-	}
-
 	switch info.Template {
 	case "simple":
 		return initSimple(cmd, path, info, c)
 	case "static":
 		return initStatic(cmd, info, options, c)
+	}
+
+	if !c.Flags().Changed("template") || !c.Flags().Changed("mode") {
+		err = cmd.selectVulcanTemplates(info)
+		if err != nil {
+			return err
+		}
 	}
 
 	shouldFetchTemplates, err := shouldFetch(cmd, info)
