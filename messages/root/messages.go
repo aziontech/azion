dd7e34fb7afe4d2931849b0bbcfcea7328dae65c
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-08-16 11:12:10 -0300

    chore: change msg from flag yes global
    Co-authored-by: Gabriel França <119522814+gabriel-azion@users.noreply.github.com>

--- messages/root/messages.go
+++ messages/root/messages.go
@@ -10,7 +10,7 @@ var (
 	RootLogSilent   = "Silences log completely; mostly used for automation purposes"
 	RootTokenFlag   = "Saves a given personal token locally to authorize CLI commands"
 	RootConfigFlag  = "Sets the Azion configuration folder for the current command only, without changing persistent settings."
-	RootYesFlag     = "yes global that says yes to everything"
+	RootYesFlag     = "Answers all yes/no interactions automatically with yes"
 	TokenSavedIn    = "Token saved in %v\n"
 	TokenUsedIn     = "This token will be used by default when calling any command"
 )
