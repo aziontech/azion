19e9a3306a147bc4a29010b43101d059399efe8b
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-08-17 15:36:08 -0300

    chore: remove legacy code

--- pkg/cmd/edge_applications/edge_applications.go
+++ pkg/cmd/edge_applications/edge_applications.go
@@ -3,13 +3,10 @@ package edge_applications
 import (
 	"github.com/MakeNowJust/heredoc"
 	msg "github.com/aziontech/azion-cli/messages/edge_applications"
-	buildCmd "github.com/aziontech/azion-cli/pkg/cmd/edge_applications/build"
 	"github.com/aziontech/azion-cli/pkg/cmd/edge_applications/delete"
 	"github.com/aziontech/azion-cli/pkg/cmd/edge_applications/describe"
-	initCmd "github.com/aziontech/azion-cli/pkg/cmd/edge_applications/init"
 	"github.com/aziontech/azion-cli/pkg/cmd/edge_applications/list"
 	"github.com/aziontech/azion-cli/pkg/cmd/edge_applications/ls"
-	publishCmd "github.com/aziontech/azion-cli/pkg/cmd/edge_applications/publish"
 	"github.com/aziontech/azion-cli/pkg/cmd/edge_applications/update"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/spf13/cobra"
@@ -28,9 +25,6 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		},
 	}
 
-	edge_applicationsCmd.AddCommand(initCmd.NewCmd(f))
-	edge_applicationsCmd.AddCommand(buildCmd.NewCmd(f))
-	edge_applicationsCmd.AddCommand(publishCmd.NewCmd(f))
 	edge_applicationsCmd.AddCommand(describe.NewCmd(f))
 	edge_applicationsCmd.AddCommand(delete.NewCmd(f))
 	edge_applicationsCmd.AddCommand(list.NewCmd(f))
