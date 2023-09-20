d38b6a97ee423dbb14a2755b0a2584339b79bd68
maxwelbm <maxwelbm@hotmail.com>
2023-09-20 17:10:36 -0300

    feat: create command describe

---
+++ messages/describe/messages.go
@@ -0,0 +1,8 @@
+package describe
+
+var (
+	Usage            = "describe"
+	ShortDescription = "Returns information about a specific configuration for each sub-command"
+	LongDescription  = "Returns information about a specific configuration for each sub-command, based on a given ID, in detail"
+	FlagHelp         = "Displays more information about the create command"
+)
---
+++ pkg/cmd/describe/describe.go
@@ -0,0 +1,24 @@
+package create
+
+import (
+	"github.com/MakeNowJust/heredoc"
+	msg "github.com/aziontech/azion-cli/messages/describe"
+	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func NewCmd(f *cmdutil.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   msg.Usage,
+		Short: msg.ShortDescription,
+		Long:  msg.LongDescription, Example: heredoc.Doc(`
+		$ azion create --help
+        `),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cmd.Help()
+		},
+	}
+
+	cmd.Flags().BoolP("help", "h", false, msg.FlagHelp)
+	return cmd
+}
