1c4d375e1af3853deccc7518d305888594aafbea
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-09-22 10:22:49 -0300

    docs: update error message "missing azion file"
    Co-authored-by: Gabriel França <119522814+gabriel-azion@users.noreply.github.com>

--- messages/delete/edge_application/errors.go
+++ messages/delete/edge_application/errors.go
@@ -3,7 +3,7 @@ package edgeapplication
 import "errors"
 
 var (
-	ErrorMissingAzionJson             = errors.New("Azion.json file is missing. Please initialize and publish your project first before using cascade delete")
+	ErrorMissingAzionJson             = errors.New("Azion.json file is missing. Please initialize and deploy your project before using cascade delete")
 	ErrorMissingApplicationIdJson     = errors.New("Application ID is missing from azion.json. Please initialize and publish your project first before using cascade delete")
 	ErrorFailToDeleteApplication      = errors.New("Failed to delete the Edge Application: %s. Check your settings and try again. If the error persists, contact Azion support")
 	ErrorMissingApplicationIdArgument = errors.New("A required flag is missing. You must provide an application_id as an argument or path to import the file. Run the command 'azioncli edge_applications <subcommand> --help' to display more information and try again")
