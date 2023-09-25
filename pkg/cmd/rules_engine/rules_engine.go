aeec42f3e7f23980c530899b5a1cc2cd3fe46205
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-09-25 12:21:12 -0300

    refactor: remove unused code

--- pkg/cmd/rules_engine/rules_engine.go
+++ pkg/cmd/rules_engine/rules_engine.go
@@ -6,7 +6,6 @@ import (
 	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/create"
 	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/delete"
 	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/describe"
-	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/list"
 	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/template"
 	"github.com/aziontech/azion-cli/pkg/cmd/rules_engine/update"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
@@ -26,7 +25,6 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		},
 	}
 
-	rulesEngineCmd.AddCommand(list.NewCmd(f))
 	rulesEngineCmd.AddCommand(describe.NewCmd(f))
 	rulesEngineCmd.AddCommand(delete.NewCmd(f))
 	rulesEngineCmd.AddCommand(update.NewCmd(f))
