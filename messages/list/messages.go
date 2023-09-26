7ef1aeba2930d15a8f55428b6d35362a8729bc73
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-09-26 14:24:25 -0300

    docs: update messages/list/messages.go
    Co-authored-by: Gabriel França <119522814+gabriel-azion@users.noreply.github.com>

--- messages/list/messages.go
+++ messages/list/messages.go
@@ -4,5 +4,5 @@ var (
 	Usage            = "list"
 	ShortDescription = "List all items of a service or resource"
 	LongDescription  = "List all items of a service or resource available on the Azion platform"
-	FlagHelp         = "Displays more information about the edge_application command"
+	FlagHelp         = "Displays more information about the list command"
 )
