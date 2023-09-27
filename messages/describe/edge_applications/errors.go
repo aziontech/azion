f1835238997238c08bfa8b8b6b83ab54cb29bf1c
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-09-27 17:04:16 -0300

    chore: text correction
    Co-authored-by: Hélio Corrêa da Silva Neto <helio.hetfield@gmail.com>

--- messages/describe/edge_applications/errors.go
+++ messages/describe/edge_applications/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	ErrorGetApplication       = errors.New("Failed to get the Edge Application: %s. Check your settings and try again. If the error persists, contact Azion support")
-	ErrorConvertIdApplication = errors.New("The application ID you provided is invalid. The value must be an integer. You may run the 'azion list edge-application' ommand to check your application ID")
+	ErrorConvertIdApplication = errors.New("The application ID you provided is invalid. The value must be an integer. You may run the 'azion list edge-application' command to check your application ID")
 )
