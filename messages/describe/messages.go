b89438f7107d5366126860bfbfb7a94dc82c69c7
maxwelbm <maxwelbm@hotmail.com>
2023-09-28 18:21:22 -0300

    chore: text and message corrections

--- messages/describe/messages.go
+++ messages/describe/messages.go
@@ -4,5 +4,5 @@ var (
 	Usage            = "describe"
 	ShortDescription = "Returns information about a specific configuration for each sub-command"
 	LongDescription  = "Returns information about a specific configuration for each sub-command, based on a given ID, in detail"
-	FlagHelp         = "Displays more information about the create command"
+	FlagHelp         = "Displays more information about the describe command"
 )
