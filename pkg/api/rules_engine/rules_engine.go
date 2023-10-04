fdefa36789522d7d27dd3a604e372f03cc3acd89
maxwelbm <maxwelbm@hotmail.com>
2023-10-04 19:44:57 -0300

    feat: created command created from rules engine

--- pkg/api/rules_engine/rules_engine.go
+++ pkg/api/rules_engine/rules_engine.go
@@ -2,11 +2,24 @@ package rules_engine
 
 import (
 	"context"
+	"io"
 
 	"github.com/aziontech/azion-cli/pkg/logger"
 	"github.com/aziontech/azion-cli/utils"
+	sdk "github.com/aziontech/azionapi-go-sdk/edgeapplications"
 	"go.uber.org/zap"
 )
+
+type RulesEngineResponse interface {
+	GetId() int64
+	GetPhase() string
+	GetDescription() string
+	GetBehaviors() []sdk.RulesEngineBehaviorEntry
+	GetCriteria() [][]sdk.RulesEngineCriteria
+	GetIsActive() bool
+	GetOrder() int64
+	GetName() string
+}
 
 func (c *Client) Delete(ctx context.Context, edgeApplicationID int64, phase string, ruleID int64) error {
 	logger.Debug("Delete Rules Engine")
@@ -22,4 +35,27 @@ func (c *Client) Delete(ctx context.Context, edgeApplicationID int64, phase stri
 		return utils.ErrorPerStatusCode(httpResp, err)
 	}
 	return nil
+}
+
+func (c *Client) Create(ctx context.Context, edgeApplicationID int64, phase string, req sdk.CreateRulesEngineRequest) (RulesEngineResponse, error) {
+	logger.Debug("Create Rules Engine")
+	resp, httpResp, err := c.apiClient.EdgeApplicationsRulesEngineAPI.
+		EdgeApplicationsEdgeApplicationIdRulesEnginePhaseRulesPost(ctx, edgeApplicationID, phase).
+		CreateRulesEngineRequest(req).Execute()
+
+	if err != nil {
+		if httpResp != nil {
+			logger.Debug("Error while updating a rules engine", zap.Error(err))
+			logger.Debug("", zap.Any("Status Code", httpResp.StatusCode))
+			logger.Debug("", zap.Any("Headers", httpResp.Header))
+			body, errReadAll := io.ReadAll(httpResp.Body)
+			if errReadAll != nil {
+				logger.Debug("Error while reading body of the http response", zap.Error(errReadAll))
+				return nil, utils.ErrorPerStatusCode(httpResp, errReadAll)
+			}
+			logger.Debug("", zap.Any("Body", string(body)))
+		}
+		return nil, utils.ErrorPerStatusCode(httpResp, err)
+	}
+	return &resp.Results, nil
 }
