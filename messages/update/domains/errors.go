6bc780c6394ec70df4ad68ddb0ea52ac88db6fba
maxwelbm <maxwelbm@hotmail.com>
2023-10-09 15:26:19 -0300

    chore: message corrections

--- messages/update/domains/errors.go
+++ messages/update/domains/errors.go
@@ -6,5 +6,5 @@ var (
 	ErrorUpdateDomain           = errors.New("Failed to update the Domain: %s. Check your settings and try again. If the error persists, contact Azion support.")
 	ErrorActiveFlag             = errors.New("Invalid --active flag provided. The flag must have  'true' or 'false' values. Run the command 'azion domains <subcommand> --help' to display more information and try again.")
 	ErrorDigitalCertificateFlag = errors.New("Invalid --digital-certificate-id flag provided. The flag must have an Integer or 'null' as a value. Run the command 'azion domains <subcommand> --help' to display more information and try again")
-	ErrorConvertDomainID        = errors.New("The domain ID you provided is invalid. The value must be an integer. You may run the 'azion list edge-application' command to check your application ID")
+	ErrorConvertDomainID        = errors.New("The domain ID you provided is invalid. The value must be an integer. You may run the 'azion list domains' command to check your domain ID")
 )
--- messages/update/domains/messages.go
+++ messages/update/domains/messages.go
@@ -4,7 +4,7 @@ var (
 	Usage                    = "domains --domain-id <domain_id> [flags]"
 	ShortDescription         = "Updates a Domain"
 	LongDescription          = "Updates a Domain based on its ID to update its name and other attributes"
-	FlagDomainID             = "Unique identifier of the Domain"
+	FlagDomainID             = "The '--domain-id' flag is mandatory"
 	FlagDigitalCertificateID = "Unique identifier of the Digital Certificate; this value is either an integer or null"
 	FlagApplicationID        = "Unique identifier for an edge application used by this domain.. The '--application-id' flag is mandatory"
 	FlagName                 = "The Domain's name"
@@ -13,6 +13,6 @@ var (
 	FlagIn                   = "Given path and JSON file to automatically update the Domain attributes; you can use - for reading from stdin"
 	OutputSuccess            = "Updated Domain with ID %d\n"
 	FlagActive               = "Whether the Domain should be active or not"
-	HelpFlag                 = "Displays more information about the update subcommand"
+	HelpFlag                 = "Displays more information about the update domains subcommand"
 	AskInputDomainID         = "What is the ID of the domain?"
 )
