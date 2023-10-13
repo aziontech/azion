0451da1476e9cf8de30c3db3dd154a8b83677c2a
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-10-13 17:10:47 +0000

    chore: update message
    Co-authored-by: Hélio Corrêa da Silva Neto <helio.hetfield@gmail.com>

--- messages/describe/domains/errors.go
+++ messages/describe/domains/errors.go
@@ -2,4 +2,4 @@ package domains
 
 import "errors"
 
-var ErrorGetDomain = errors.New("Failed to your domain. Check your settings and try again. If the error persists, contact Azion support.")
+var ErrorGetDomain = errors.New("Failed to describe your domain. Check your settings and try again. If the error persists, contact Azion support.")
