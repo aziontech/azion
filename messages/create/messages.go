72762ac2b788ad386f13bdda29a9c178404846c0
gabriel-azion <gabriel.franca@azion.com>
2023-10-20 11:46:09 -0300

    docs: review root help messages

--- messages/create/messages.go
+++ messages/create/messages.go
@@ -2,7 +2,7 @@ package create
 
 var (
 	Usage            = "create"
-	ShortDescription = "The create command is literally for creating new items"
-	LongDescription  = "The create command is literally for creating new items, requiring any of several sub-commands to create the chosen item"
+	ShortDescription = "Creates a new resource in the Azion Edge Platform"
+	LongDescription  = "Creates a new resource in the Azion Edge Platform, requiring any of several sub-commands to create the chosen item"
 	FlagHelp         = "Displays more information about the create command"
 )
--- messages/dev/messages.go
+++ messages/dev/messages.go
@@ -3,6 +3,6 @@ package dev
 var (
 	DevFlagHelp         = "Displays more information about the dev command"
 	DevUsage            = "dev [flags]"
-	DevShortDescription = "Starts a local development server"
-	DevLongDescription  = "Start a development server locally"
+	DevShortDescription = "Starts a local development server for the current application"
+	DevLongDescription  = "Starts a local development server for the current application, so it's possible to preview and test it locally before the deployment"
 )
