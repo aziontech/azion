bf322d672920f8614a5aef55371187e51f95ceee
gabriel-azion <gabriel.franca@azion.com>
2023-10-23 09:43:13 -0300

    capitalization for instances x product

--- messages/create/rules_engine/messages.go
+++ messages/create/rules_engine/messages.go
@@ -9,8 +9,8 @@ var (
 	FlagPhase             = "The phase is either 'request' or 'response'"
 	FlagIn                = "Path to a JSON file containing the attributes of the rule that will be created; you can use - for reading from stdin"
 	OutputSuccess         = "Created Rules Engine with ID %d\n"
-	HelpFlag              = "Displays more information about the create subcommand"
-	AskInputApplicationId = "What is the ID of the Edge Application that the Rule Engine will be connected to?"
+	HelpFlag              = "Displays more information about the azion create rules-engine subcommand"
+	AskInputApplicationId = "What is the ID of the edge application that the rules engine will be connected to?"
 	AskInputPhase         = "What is the phase of your rule engine? (request/response)"
 	AskInputPathFile      = "What is the path of the json to create the rules engine?"
 )
