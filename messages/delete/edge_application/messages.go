428b16bf2ddbcaaafdd8492529345378d68d6920
gabriel-azion <gabriel.franca@azion.com>
2023-10-23 09:26:18 -0300

    review delete messages

--- messages/delete/edge_application/messages.go
+++ messages/delete/edge_application/messages.go
@@ -2,12 +2,12 @@ package edgeapplication
 
 var (
 	Usage            = "edge-application"
-	ShortDescription = "Removes an Edge Application"
-	LongDescription  = "Removes an Edge Application from the Edge Applications library based on its given ID"
-	OutputSuccess    = "Edge Application %d was successfully deleted\n"
+	ShortDescription = "Removes an edge application"
+	LongDescription  = "Removes an edge application from the Edge Applications library based on a given ID"
+	OutputSuccess    = "Edge application %d was successfully deleted\n"
 	HelpFlag         = "Displays more information about the delete edge-application command"
 	CascadeFlag      = "Deletes all resources created through the 'azion deploy' command"
 	MissingFunction  = "Missing Edge Function ID in azion.json file. Skipping deletion"
 	CascadeSuccess   = "Cascade delete carried out successfully"
-	FlagId           = "Unique identifier of the Edge Application"
+	FlagId           = "Unique identifier of the edge application"
 )
