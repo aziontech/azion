591ddba59f923a9f29e0c3e1371edfc01c6d1a8d
Gabriel França <119522814+gabriel-azion@users.noreply.github.com>
2023-10-23 11:28:53 -0300

    Update messages/delete/messages.go

--- messages/delete/messages.go
+++ messages/delete/messages.go
@@ -3,6 +3,6 @@ package delete
 var (
 	Usage            = "delete"
 	ShortDescription = "Deletes a resource"
-	LongDescription  = "Deletes a resource based on the given ID information"
+	LongDescription  = "Deletes a resource based on the given ID"
 	FlagHelp         = "Displays more information about the delete command"
 )
