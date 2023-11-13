73439a7986244d4a813a651da8016a716b99cb45
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-11-13 11:44:46 -0300

    chore: add subcommand to help messages

--- messages/list/messages.go
+++ messages/list/messages.go
@@ -1,7 +1,7 @@
 package list
 
 var (
-	Usage            = "list [flags]"
+	Usage            = "list <subcommand> [flags]"
 	ShortDescription = "Lists all services or resources you have access to on the platform"
 	LongDescription  = "Lists all services or resources you have access to on the platform available on the Azion platform"
 	FlagHelp         = "Displays more information about the list command"
