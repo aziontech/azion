833280cf525e7d972d83c8c06e7b3ad159b08ae5
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-11-20 16:00:37 -0300

    refactor: update logout message

--- messages/logout/messages.go
+++ messages/logout/messages.go
@@ -5,7 +5,7 @@ const (
 	Usage            = "logout"
 	ShortDescription = "Log out of your Azion account"
 	LongDescription  = "Log out of your Azion account and remove the personal token saved locally"
-	Success          = "successfully logout"
+	Success          = "successfully logged out"
 
 	// flags
 	FlagHelp = "Displays more information about the logout command"
