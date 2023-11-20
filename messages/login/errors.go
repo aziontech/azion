645790bec0dc8945b89312ea1081b7b75c08ba01
maxwelbm <maxwelbm@hotmail.com>
2023-11-20 10:36:04 -0300

    chore: improvement of the error message token created is invalid and the login description

--- messages/login/errors.go
+++ messages/login/errors.go
@@ -2,5 +2,5 @@ package login
 
 const (
 	ErrorLogin              = "Failed to Login: %s. Check your settings and try again. If the error persists, contact Azion support."
-	ErrorTokenCreateInvalid = "The token was created, but it is invalid"
+	ErrorTokenCreateInvalid = "Invalid token detected. The generated token appears to be corrupted or expired. Please check your authentication credentials and generate a new token to proceed."
 )
