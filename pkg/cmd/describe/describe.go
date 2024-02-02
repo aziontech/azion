d9518be2cc9811007e35196dafdcf2bd11e61461
maxwelbm <maxwelbm@hotmail.com>
2024-02-02 15:17:54 -0300

    feat: command describe variables

--- pkg/cmd/describe/describe.go
+++ pkg/cmd/describe/describe.go
@@ -9,6 +9,7 @@ import (
 	function "github.com/aziontech/azion-cli/pkg/cmd/describe/edge_function"
 	origin "github.com/aziontech/azion-cli/pkg/cmd/describe/origin"
 	ruleEngine "github.com/aziontech/azion-cli/pkg/cmd/describe/rules_engine"
+	"github.com/aziontech/azion-cli/pkg/cmd/describe/variables"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -21,9 +22,10 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 		Example: heredoc.Doc(`
 		$ azion describe --help
 		$ azion describe edge-application
-		$ azion describe domain 
-		$ azion describe origin 
-		$ azion describe rule-engine 
+		$ azion describe domain
+		$ azion describe origin
+		$ azion describe rule-engine
+		$ azion describe variables
         `),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return cmd.Help()
@@ -36,6 +38,7 @@ func NewCmd(f *cmdutil.Factory) *cobra.Command {
 	cmd.AddCommand(origin.NewCmd(f))
 	cmd.AddCommand(cache.NewCmd(f))
 	cmd.AddCommand(function.NewCmd(f))
+	cmd.AddCommand(variables.NewCmd(f))
 
 	cmd.Flags().BoolP("help", "h", false, msg.FlagHelp)
 	return cmd
--- pkg/cmd/describe/variables/variables_test.go
+++ pkg/cmd/describe/variables/variables_test.go
@@ -1,4 +1,4 @@
-package describe
+package variables
 
 import (
 	"log"
