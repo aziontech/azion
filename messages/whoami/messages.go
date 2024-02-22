2259b5138469d26933c65df8aae4a5f000192ce7
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-02-22 15:51:45 -0300

    refactor: update whoami messages

--- messages/whoami/messages.go
+++ messages/whoami/messages.go
@@ -2,7 +2,7 @@ package whoami
 
 var (
 	Usage            = "whoami"
-	ShortDescription = "Shows user currently logged in"
-	LongDescription  = "Shows email of the user currently logged in"
+	ShortDescription = "Displays user currently logged in"
+	LongDescription  = "Displays email of the user currently logged in"
 	HelpFlag         = "Displays more information about the 'whoami' command"
 )
