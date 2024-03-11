e9ba1637b72f17eba64fbc22979e0651318a7d19
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-03-11 13:56:42 -0300

    chore: display new version on update message

--- pkg/cmd/root/sysinfo_darwin.go
+++ pkg/cmd/root/sysinfo_darwin.go
@@ -3,12 +3,14 @@
 package root
 
 import (
+	"fmt"
+
 	msg "github.com/aziontech/azion-cli/messages/root"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/aziontech/azion-cli/pkg/logger"
 )
 
-func showUpdadeMessageSystem(f *cmdutil.Factory) error {
-	logger.FInfo(f.IOStreams.Out, msg.BrewUpdate)
+func showUpdadeMessageSystem(f *cmdutil.Factory, vNumber string) error {
+	logger.FInfo(f.IOStreams.Out, fmt.Sprintf(msg.BrewUpdate, vNumber))
 	return nil
 }
--- pkg/cmd/root/sysinfo_freebsd.go
+++ pkg/cmd/root/sysinfo_freebsd.go
@@ -3,13 +3,15 @@
 package root
 
 import (
+	"fmt"
+
 	msg "github.com/aziontech/azion-cli/messages/root"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/aziontech/azion-cli/pkg/logger"
 )
 
-func showUpdadeMessageSystem(f *cmdutil.Factory) error {
+func showUpdadeMessageSystem(f *cmdutil.Factory, vNumber string) error {
 	logger.FInfo(f.IOStreams.Out, msg.DownloadRelease)
-	logger.FInfo(f.IOStreams.Out, msg.PkgUpdate)
+	logger.FInfo(f.IOStreams.Out, fmt.Sprintf(msg.PkgUpdate, vNumber))
 	return nil
 }
