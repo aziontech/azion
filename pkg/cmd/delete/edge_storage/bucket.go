dbb101376251ae8d997fb32fc6aed06266d34c26
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-03-15 10:37:39 -0300

    refactor: add delete command usage

--- pkg/cmd/delete/edge_storage/bucket.go
+++ pkg/cmd/delete/edge_storage/bucket.go
@@ -17,27 +17,27 @@ var name string
 
 func NewBucket(f *cmdutil.Factory) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           msg.USAGE,
+		Use:           msg.USAGE_BUCKET,
 		Short:         msg.SHORT_DESCRIPTION_DELETE_BUCKET,
 		Long:          msg.LONG_DESCRIPTION_DELETE_BUCKET,
 		SilenceUsage:  true,
 		SilenceErrors: true,
-		Example: heredoc.Doc(msg.EXAMPLE_DELETE_BUCKET),
-		RunE: runE(f),
+		Example:       heredoc.Doc(msg.EXAMPLE_DELETE_BUCKET),
+		RunE:          runE(f),
 	}
 	cmd.Flags().StringVar(&name, "name", "", msg.FLAG_NAME_BUCKET)
-	cmd.Flags().BoolP("help", "h", false, msg.FLAG_HELP_DELETE_BUCKET)	
+	cmd.Flags().BoolP("help", "h", false, msg.FLAG_HELP_DELETE_BUCKET)
 	return cmd
 }
 
 func runE(f *cmdutil.Factory) func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		if !cmd.Flags().Changed("name") {	
+		if !cmd.Flags().Changed("name") {
 			answer, err := utils.AskInput(msg.ASK_NAME_DELETE_BUCKET)
 			if err != nil {
 				return err
 			}
-			name = answer 
+			name = answer
 		}
 		client := api.NewClient(f.HttpClient, f.Config.GetString("storage_url"), f.Config.GetString("token"))
 		ctx := context.Background()
