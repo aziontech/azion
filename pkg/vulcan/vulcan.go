7e7066588e50e54a610ddb46decdb11558d28d72
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-03-28 09:57:56 -0300

    refactor: update vulcan version

--- pkg/vulcan/vulcan.go
+++ pkg/vulcan/vulcan.go
@@ -19,7 +19,7 @@ const (
 	firstTimeExecuting   = "@v2.5.0"
 )
 
-var versionVulcan = "@v2.6.0-stage.7"
+var versionVulcan = "@latest"
 
 func Command(flags, params string) string {
 	return fmt.Sprintf(installEdgeFunctions, flags, versionVulcan, params)
