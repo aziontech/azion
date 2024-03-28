012c6582f2d871574b066ef8e93073cff74e612f
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2024-03-28 12:04:25 -0300

    refactor: update message
    Co-authored-by: Pablo Diehl <pablodiehl@ymail.com>

--- messages/edge_storage/messages.go
+++ messages/edge_storage/messages.go
@@ -10,7 +10,7 @@ const (
 	SHORT_DESCRIPTION_LIST_BUCKET    = "List the buckets in Edge Storage"
 	SHORT_DESCRIPTION_LIST_OBJECT    = "List the objects in Edge Storage"
 	SHORT_DESCRIPTION_DELETE_BUCKET  = "Deleted the bucket in Edge Storage"
-	SHORT_DESCRIPTION_CREATE_OBJECTS = "Creates a objects in Edge Storage"
+	SHORT_DESCRIPTION_CREATE_OBJECTS = "Creates objects in Edge Storage"
 
 	LONG_DESCRIPTION                = "Allows users to perform a wide range of operations, from creating and deleting buckets to adding, removing, and manipulating objects."
 	LONG_DESCRIPTION_CREATE_BUCKET  = "Allows users to create a bucket in Edge Storage"
