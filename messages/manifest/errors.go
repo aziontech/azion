2baf15bdd8cd14e0baf476276119cb097011e39c
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-04-25 18:04:35 -0300

    refactor: update error messages

--- messages/manifest/errors.go
+++ messages/manifest/errors.go
@@ -4,6 +4,6 @@ import "errors"
 
 var (
 	ErrorUnmarshalAzionJsonFile = errors.New("Failed to parse the given 'azion.json' file. Verify if the file format is JSON or fix its content according to the JSON format specification at https://www.json.org/json-en.html")
-	ErrorCacheNotFound          = errors.New("Could not Find this cache setting")
-	ErrorOriginNotFound         = errors.New("Could not Find this origin")
+	ErrorCacheNotFound          = errors.New("Could not find this cache setting")
+	ErrorOriginNotFound         = errors.New("Could not find this origin")
 )
