a8eadbda1153fc467ba0c978fa3ff589823f9430
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-06-03 12:03:38 -0300

    refactor: fix success message

--- messages/reset/messages.go
+++ messages/reset/messages.go
@@ -5,5 +5,5 @@ const (
 	SHORTDESCRIPTION = "Resets settings to default"
 	LONGDESCRIPTION  = "Resets your settings to default and deletes personal token if possible"
 	FLAGHELP         = "Displays more information about the reset command"
-	SUCCESS          = "Sucessfully reseted to default settings"
+	SUCCESS          = "Sucessfully reset to default settings"
 )
