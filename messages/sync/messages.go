fdb2805c45903baf54790aa145c2202eaed2df5c
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-06-11 14:31:14 -0300

    docs: update messages for sync command
    Co-authored-by: Gabriel França <119522814+gabriel-azion@users.noreply.github.com>

--- messages/sync/messages.go
+++ messages/sync/messages.go
@@ -3,7 +3,7 @@ package sync
 const (
 	USAGE            = "sync"
 	SHORTDESCRIPTION = "Synchronizes the local azion.json file with remote resources"
-	LONGDESCRIPTION  = "Synchronizes your local file containing your created resources with remote resources"
+	LONGDESCRIPTION  = "Synchronizes your local file containing your existing application resources configuration with remote resources"
 	SYNCMESSAGERULE  = "Adding out of sync rule '%s' to your azion.json file\n"
 	HELPFLAG         = "Displays more information about the sync command"
 )
