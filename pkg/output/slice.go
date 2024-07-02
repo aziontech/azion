1c22fd2e191b7c09fe8c6a775087b1b71e43f644
maxwelbm <maxwelbm@hotmail.com>
2024-07-02 14:15:18 -0300

    refactor: lint

--- pkg/output/slice.go
+++ pkg/output/slice.go
@@ -9,7 +9,7 @@ func (i *SliceOutput) Format() (bool, error) {
 	formated := false
 	if len(i.Flags.Format) > 0 || len(i.Flags.Out) > 0 {
 		formated = true
-		err := format(i, *&i.GeneralOutput)
+		err := format(i, i.GeneralOutput)
 		if err != nil {
 			return formated, err
 		}
