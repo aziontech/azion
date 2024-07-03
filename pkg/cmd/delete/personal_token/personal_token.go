cdcca687bd13b54e0aab6fbfaf278cbc81a10117
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-07-03 16:26:30 -0300

    tests: add unit tests for delete personal token

--- pkg/cmd/delete/personal_token/personal_token.go
+++ pkg/cmd/delete/personal_token/personal_token.go
@@ -8,57 +8,81 @@ import (
 	msg "github.com/aziontech/azion-cli/messages/delete/personal_token"
 	api "github.com/aziontech/azion-cli/pkg/api/personal_token"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/aziontech/azion-cli/pkg/iostreams"
 	"github.com/aziontech/azion-cli/pkg/output"
 	"github.com/aziontech/azion-cli/utils"
 	"github.com/spf13/cobra"
 )
 
-func NewCmd(f *cmdutil.Factory) *cobra.Command {
-	var id string
+var (
+	tokenID string
+)
 
-	deleteCmd := &cobra.Command{
+type DeleteCmd struct {
+	Io         *iostreams.IOStreams
+	AskInput   func(string) (string, error)
+	DeleteFunc func(context.Context, string) error
+}
+
+func NewDeleteCmd(f *cmdutil.Factory) *DeleteCmd {
+	return &DeleteCmd{
+		Io: f.IOStreams,
+		AskInput: func(prompt string) (string, error) {
+			return utils.AskInput(prompt)
+		},
+		DeleteFunc: func(ctx context.Context, id string) error {
+			client := api.NewClient(f.HttpClient, f.Config.GetString("api_url"), f.Config.GetString("token"))
+			return client.Delete(ctx, id)
+		},
+	}
+}
+
+func NewCobraCmd(delete *DeleteCmd, f *cmdutil.Factory) *cobra.Command {
+	cobraCmd := &cobra.Command{
 		Use:           msg.Usage,
 		Short:         msg.ShortDescription,
 		Long:          msg.LongDescription,
-		SilenceErrors: true,
 		SilenceUsage:  true,
+		SilenceErrors: true,
 		Example: heredoc.Doc(`
-		$ azion delete personal-token --id 1234-123-321
+		  $ azion delete personal-token --id 1234-123-321
 		`),
-
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+
 			if !cmd.Flags().Changed("id") {
-				answer, err := utils.AskInput(msg.AskDeleteInput)
+				answer, err := delete.AskInput(msg.AskDeleteInput)
 				if err != nil {
 					return err
 				}
-
-				id = answer
+				tokenID = answer
 			}
 
-			if utils.IsEmpty(id) {
+			if utils.IsEmpty(tokenID) {
 				return utils.ErrorArgumentIsEmpty
 			}
 
-			client := api.NewClient(f.HttpClient, f.Config.GetString("api_url"), f.Config.GetString("token"))
+			ctx := context.Background()
 
-			err := client.Delete(context.Background(), id)
+			err = delete.DeleteFunc(ctx, tokenID)
 			if err != nil {
 				return fmt.Errorf(msg.ErrorFailToDelete.Error(), err)
 			}
 
 			deleteOut := output.GeneralOutput{
-				Msg:   fmt.Sprintf(msg.OutputSuccess, id),
+				Msg:   fmt.Sprintf(msg.OutputSuccess, tokenID),
 				Out:   f.IOStreams.Out,
 				Flags: f.Flags,
 			}
 			return output.Print(&deleteOut)
-
 		},
 	}
 
-	deleteCmd.Flags().StringVar(&id, "id", "", msg.FlagID)
-	deleteCmd.Flags().BoolP("help", "h", false, msg.HelpFlag)
+	cobraCmd.Flags().StringVar(&tokenID, "id", "", msg.FlagID)
+	cobraCmd.Flags().BoolP("help", "h", false, msg.HelpFlag)
+	return cobraCmd
+}
 
-	return deleteCmd
+func NewCmd(f *cmdutil.Factory) *cobra.Command {
+	return NewCobraCmd(NewDeleteCmd(f), f)
 }
