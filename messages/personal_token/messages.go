97e2adb749a25915fd4fadb42e215c850933b633
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2024-07-10 18:11:08 -0300

    chore: better message text

--- messages/personal_token/messages.go
+++ messages/personal_token/messages.go
@@ -5,6 +5,6 @@ const (
 	SHORT_DESCRIPTION_DESCRIBE = "Describe a Personal Token"
 	LONG_DESCRIPTION_DESCRIBE  = "Describe a Personal Token's to be used for authentication and security"
 	ASK_PERSONAL_TOKEN_ID      = "Enter the Personal Token's ID:"
-	FLAG_PERSONAL_TOKEN_ID     = "Unique identifier for an Personal Token"
+	FLAG_PERSONAL_TOKEN_ID     = "Unique identifier for a Personal Token"
 	FLAG_HELP_DESCRIBE         = "Displays more information about the describe personal-token subcommand"
 )
