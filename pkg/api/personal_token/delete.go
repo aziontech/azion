2a1543c51f6bf111107c82fbe9693db03a4856a4
maxwelbm <maxwelbm@hotmail.com>
2024-07-10 13:18:21 -0300

    feat: api personal token

--- pkg/api/personal_token/delete.go
+++ pkg/api/personal_token/delete.go
@@ -10,12 +10,16 @@ import (
 
 func (c *Client) Delete(ctx context.Context, id string) error {
 	logger.Debug("Delete personal token")
-
 	httpResp, err := c.apiClient.PersonalTokenApi.DeletePersonalToken(ctx, id).Execute()
 	if err != nil {
-		logger.Debug("Error while deleting a personal token", zap.Error(err))
+		if httpResp != nil {
+			logger.Debug("Error while delete your personal token", zap.Error(err))
+			err := utils.LogAndRewindBody(httpResp)
+			if err != nil {
+				return err
+			}
+		}
 		return utils.ErrorPerStatusCode(httpResp, err)
 	}
-
 	return nil
 }
