245a4e5579d5da1c2a484a3c4d01290ded0902be
maxwelbm <maxwelbm@hotmail.com>
2024-07-10 17:45:54 -0300

    chore: update messages

--- messages/personal_token/messages.go
+++ messages/personal_token/messages.go
@@ -2,9 +2,9 @@ package personal_token
 
 const (
 	USAGE                      = "personal-token"
-	SHORT_DESCRIPTION_DESCRIBE = "Describe a Personal Token's"
+	SHORT_DESCRIPTION_DESCRIBE = "Describe a Personal Token"
 	LONG_DESCRIPTION_DESCRIBE  = "Describe a Personal Token's to be used for authentication and security"
 	ASK_PERSONAL_TOKEN_ID      = "Enter the Personal Token's ID:"
-	FLAG_PERSONAL_TOKEN_ID     = "Unique identifier for an Personal Token's"
+	FLAG_PERSONAL_TOKEN_ID     = "Unique identifier for an Personal Token"
 	FLAG_HELP_DESCRIBE         = "Displays more information about the describe personal-token subcommand"
 )
