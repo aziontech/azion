95746b654d7a90f34ff022be4f656a671a5361dd
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-08-16 10:03:48 -0300

    test: remove flags from vulcan test

--- scripts/test.sh
+++ scripts/test.sh
@@ -46,8 +46,8 @@ if [ -f "$full_main_go_path" ]; then
         check_folder_exists "$expected_folder"
 
         # Run the build command
-        echo "Running cmd/azion/main.go build --preset react --mode deliver --debug"
-        go run "$full_main_go_path" build --preset react --mode deliver --debug
+        echo "Running cmd/azion/main.go build --debug"
+        go run "$full_main_go_path" build --debug
 
         # Check the exit status of the build command
         if [ $? -eq 0 ]; then
