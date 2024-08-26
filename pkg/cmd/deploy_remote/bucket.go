a90b1e7c34ac8e970b989006d489ab6a8cd565a0
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-08-26 14:48:44 -0300

    chore: add deploy-remote command

---
+++ pkg/cmd/deploy_remote/bucket.go
@@ -0,0 +1,81 @@
+package deploy
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/aziontech/azionapi-go-sdk/storage"
+
+	"github.com/AlecAivazis/survey/v2"
+	msg "github.com/aziontech/azion-cli/messages/deploy"
+	api "github.com/aziontech/azion-cli/pkg/api/storage"
+	"github.com/aziontech/azion-cli/pkg/contracts"
+	"github.com/aziontech/azion-cli/pkg/logger"
+	"github.com/aziontech/azion-cli/utils"
+)
+
+func (cmd *DeployCmd) doBucket(
+	client *api.Client,
+	ctx context.Context,
+	conf *contracts.AzionApplicationOptions,
+	msgs *[]string) error {
+	if conf.Bucket != "" || (conf.Preset == "javascript" || conf.Preset == "typescript") {
+		return nil
+	}
+
+	logger.FInfoFlags(cmd.Io.Out, msg.ProjectNameMessage, cmd.F.Format, cmd.F.Out)
+	*msgs = append(*msgs, msg.ProjectNameMessage)
+	nameBucket := replaceInvalidChars(conf.Name)
+
+	err := client.CreateBucket(ctx, api.RequestBucket{
+		BucketCreate: storage.BucketCreate{Name: nameBucket, EdgeAccess: storage.READ_WRITE}})
+	if err != nil {
+		// If the name is already in use, try 10 times with different names
+		for i := 0; i < 10; i++ {
+			nameB := fmt.Sprintf("%s-%s", nameBucket, utils.Timestamp())
+			msgf := fmt.Sprintf(msg.NameInUseBucket, nameB)
+			logger.FInfoFlags(cmd.Io.Out, msgf, cmd.F.Format, cmd.F.Out)
+			*msgs = append(*msgs, msgf)
+			err := client.CreateBucket(ctx, api.RequestBucket{
+				BucketCreate: storage.BucketCreate{Name: nameB, EdgeAccess: storage.READ_WRITE}})
+			if err != nil {
+				if errors.Is(err, utils.ErrorNameInUse) && i < 9 {
+					continue
+				}
+				return err
+			}
+			conf.Bucket = nameB
+			break
+		}
+	} else {
+		conf.Bucket = nameBucket
+	}
+
+	msgf := fmt.Sprintf(msg.BucketSuccessful, conf.Bucket)
+	logger.FInfoFlags(cmd.Io.Out, msgf, cmd.F.Format, cmd.F.Out)
+	*msgs = append(*msgs, msgf)
+	return cmd.WriteAzionJsonContent(conf, ProjectConf)
+}
+
+func askForInput(msg string, defaultIn string) (string, error) {
+	var userInput string
+	prompt := &survey.Input{
+		Message: msg,
+		Default: defaultIn,
+	}
+
+	// Prompt the user for input
+	err := survey.AskOne(prompt, &userInput, survey.WithKeepFilter(true))
+	if err != nil {
+		return "", err
+	}
+	return userInput, nil
+}
+
+// replaceInvalidChars Regular expression to find disallowed characters: "[^a-zA-Z0-9]+" replace invalid characters with -
+func replaceInvalidChars(str string) string {
+	re := regexp.MustCompile(`(?i)(?:azion-|b2-)|[^a-z0-9\-]`)
+	return re.ReplaceAllString(str, "")
+}
---
+++ pkg/cmd/deploy_remote/upload.go
@@ -0,0 +1,123 @@
+package deploy
+
+import (
+	"os"
+	"strings"
+
+	msg "github.com/aziontech/azion-cli/messages/deploy"
+	"github.com/aziontech/azion-cli/pkg/api/storage"
+	"github.com/aziontech/azion-cli/pkg/cmdutil"
+	"github.com/aziontech/azion-cli/pkg/contracts"
+	"github.com/aziontech/azion-cli/pkg/logger"
+	"github.com/schollz/progressbar/v3"
+	"github.com/zRedShift/mimemagic"
+	"go.uber.org/zap"
+)
+
+var (
+	PathStatic = ".edge/storage"
+	Jobs       chan contracts.FileOps
+	Retries    int64
+)
+
+func (cmd *DeployCmd) uploadFiles(
+	f *cmdutil.Factory, conf *contracts.AzionApplicationOptions, msgs *[]string) error {
+	// Get total amount of files to display progress
+	totalFiles := 0
+	if err := cmd.FilepathWalk(PathStatic, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			logger.Debug("Error while reading files to be uploaded", zap.Error(err))
+			logger.Debug("File that caused the error: " + PathStatic)
+			return err
+		}
+		if !info.IsDir() {
+			totalFiles++
+		}
+		return nil
+	}); err != nil {
+		logger.Debug("Error while reading files to be uploaded", zap.Error(err))
+		return err
+	}
+
+	clientUpload := storage.NewClient(cmd.F.HttpClient, cmd.F.Config.GetString("storage_url"), cmd.F.Config.GetString("token"))
+
+	logger.FInfoFlags(cmd.F.IOStreams.Out, msg.UploadStart, f.Format, f.Out)
+	*msgs = append(*msgs, msg.UploadStart)
+
+	noOfWorkers := 5
+	var currentFile int64
+	Jobs := make(chan contracts.FileOps, totalFiles)
+	results := make(chan error, noOfWorkers)
+
+	// Create worker goroutines
+	for i := 1; i <= noOfWorkers; i++ {
+		go worker(Jobs, results, &currentFile, clientUpload, conf)
+	}
+
+	bar := progressbar.NewOptions(
+		totalFiles,
+		progressbar.OptionSetDescription("Uploading files"),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetWriter(cmd.F.IOStreams.Out),
+		progressbar.OptionClearOnFinish(),
+	)
+
+	if f.Silent {
+		bar = nil
+	}
+
+	if err := cmd.FilepathWalk(PathStatic, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			fileContent, err := cmd.Open(path)
+			if err != nil {
+				logger.Debug("Error while trying to read file <"+path+"> about to be uploaded", zap.Error(err))
+				return err
+			}
+
+			fileString := strings.TrimPrefix(path, PathStatic)
+			mimeType, err := mimemagic.MatchFilePath(path, -1)
+			if err != nil {
+				logger.Debug("Error while matching file path", zap.Error(err))
+				return err
+			}
+			fileOptions := contracts.FileOps{
+				Path:        fileString,
+				MimeType:    mimeType.MediaType(),
+				FileContent: fileContent,
+			}
+
+			Jobs <- fileOptions
+		}
+		return nil
+	}); err != nil {
+		logger.Debug("Error while reading files to be uploaded", zap.Error(err))
+		return err
+	}
+	close(Jobs)
+
+	// Check for errors from workers
+	for a := 1; a <= totalFiles; a++ {
+		result := <-results
+		if result != nil {
+			return result
+		}
+
+		if bar != nil {
+			err := bar.Set(int(currentFile))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// All jobs are processed, no more values will be sent on results:
+	close(results)
+	logger.FInfoFlags(cmd.F.IOStreams.Out, msg.UploadSuccessful, f.Format, f.Out)
+	*msgs = append(*msgs, msg.UploadSuccessful)
+
+	return nil
+}
