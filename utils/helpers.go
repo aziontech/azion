e80b9aefce7b00debb4fb3b17600a96242e24e99
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-11-07 15:16:51 -0300

    refactor: remove debug print

--- utils/helpers.go
+++ utils/helpers.go
@@ -646,7 +646,6 @@ func FileExists(filename string) bool {
 
 func ContainSubstring(word string, words []string) bool {
 	for _, item := range words {
-		fmt.Println(strings.Contains(item, word), word)
 		if item == word || strings.Contains(word, item) {
 			return true // Return immediately if a match is found
 		}
