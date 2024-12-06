0298d3a67648729ecb4b4ac68a0d58bf5da80a08
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2024-12-06 14:23:41 -0300

    feat: schedule bucket deletion on cascade delete

--- pkg/cmd/delete/edge_application/cascade.go
+++ pkg/cmd/delete/edge_application/cascade.go
@@ -9,6 +9,7 @@ import (
 	msg "github.com/aziontech/azion-cli/messages/delete/edge_application"
 	app "github.com/aziontech/azion-cli/pkg/api/edge_applications"
 	fun "github.com/aziontech/azion-cli/pkg/api/edge_function"
+	store "github.com/aziontech/azion-cli/pkg/cmd/delete/edge_storage/bucket"
 	"github.com/aziontech/azion-cli/pkg/output"
 )
 
@@ -26,6 +27,7 @@ func CascadeDelete(ctx context.Context, del *DeleteCmd) error {
 	}
 	clientapp := app.NewClient(del.f.HttpClient, del.f.Config.GetString("api_url"), del.f.Config.GetString("token"))
 	clientfunc := fun.NewClient(del.f.HttpClient, del.f.Config.GetString("api_url"), del.f.Config.GetString("token"))
+	storagecmd := store.NewBucket(del.f)
 
 	err = clientapp.Delete(ctx, azionJson.Application.ID)
 	if err != nil {
@@ -41,6 +43,14 @@ func CascadeDelete(ctx context.Context, del *DeleteCmd) error {
 		err = clientfunc.Delete(ctx, azionJson.Function.ID)
 		if err != nil {
 			return fmt.Errorf(msg.ErrorFailToDeleteApplication.Error(), err)
+		}
+	}
+
+	if azionJson.Bucket != "" {
+		storagecmd.SetArgs([]string{"", "--name", azionJson.Bucket, "--force"})
+		err := storagecmd.Execute()
+		if err != nil {
+			return msg.ErrorFailCascadeStorage
 		}
 	}
 
