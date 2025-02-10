5052e20c143c9ecb055f9f30fa57622f864f75e4
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2025-02-10 11:04:45 -0300

    chore: update domain management

--- pkg/vulcan/vulcan.go
+++ pkg/vulcan/vulcan.go
@@ -15,10 +15,10 @@ import (
 const (
 	currentMajor         = 4
 	installEdgeFunctions = "npx --yes %s edge-functions%s %s"
-	firstTimeExecuting   = "@4.6.1-stage.1"
+	firstTimeExecuting   = "@4.6.1"
 )
 
-var versionVulcan = "@4.6.1-stage.1"
+var versionVulcan = "@4.6.1"
 
 type VulcanPkg struct {
 	Command          func(flags, params string, f *cmdutil.Factory) string
