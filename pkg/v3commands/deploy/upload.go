ba6051b1093e47cbc18603e9271229b0e1d935c3
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2025-08-22 14:38:48 -0300

    fix: remote deploy - missing variable #patch

--- pkg/v3commands/deploy/upload.go
+++ pkg/v3commands/deploy/upload.go
@@ -1,16 +1,21 @@
 package deploy
 
 import (
+	"context"
+	"errors"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync/atomic"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	msg "github.com/aziontech/azion-cli/messages/deploy"
+	"github.com/aziontech/azion-cli/pkg/api/s3"
 	"github.com/aziontech/azion-cli/pkg/cmdutil"
 	"github.com/aziontech/azion-cli/pkg/contracts"
 	"github.com/aziontech/azion-cli/pkg/logger"
-	"github.com/aziontech/azion-cli/pkg/v3api/storage"
-	remote "github.com/aziontech/azion-cli/pkg/v3commands/deploy_remote"
+	"github.com/aziontech/azion-cli/pkg/token"
 	"github.com/schollz/progressbar/v3"
 	"github.com/zRedShift/mimemagic"
 	"go.uber.org/zap"
@@ -21,48 +26,45 @@ var (
 	Retries int64
 )
 
-func uploadFiles(f *cmdutil.Factory, conf *contracts.AzionApplicationOptionsV3, msgs *[]string, pathStatic, bucket string, cmd *DeployCmd) error {
-	// Get total amount of files to display progress
-	totalFiles := 0
-	if err := cmd.FilepathWalk(pathStatic, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			logger.Debug("Error while reading files to be uploaded", zap.Error(err))
-			logger.Debug("File that caused the error: " + pathStatic)
-			return err
-		}
-
-		// Skip node_modules and .edge directories
-		if info.IsDir() && (strings.Contains(path, "node_modules") || strings.Contains(path, ".edge")) {
-			logger.Debug("Skipping directory: " + path)
-			return filepath.SkipDir
-		}
-
-		if !info.IsDir() {
-			totalFiles++
-		}
-		return nil
-	}); err != nil {
-		logger.Debug("Error while reading files to be uploaded", zap.Error(err))
-		return err
+func uploadFiles(f *cmdutil.Factory, conf *contracts.AzionApplicationOptionsV3, msgs *[]string, pathStatic, bucket string, cmd *DeployCmd, settings token.Settings) error {
+	cfg, err := s3.New(settings.S3AccessKey, settings.S3SecreKey)
+	if err != nil {
+		return errors.New(msg.ErrorUnableSDKConfig + err.Error())
 	}
-
-	clientUpload := storage.NewClient(cmd.F.HttpClient, cmd.F.Config.GetString("storage_url"), cmd.F.Config.GetString("token"))
 
 	logger.FInfoFlags(cmd.F.IOStreams.Out, msg.UploadStart, f.Format, f.Out)
 	*msgs = append(*msgs, msg.UploadStart)
 
+	// create lots zip files
+	allFiles, err := ReadAllFiles(pathStatic, cmd)
+	if err != nil {
+		return err
+	}
+
+	err = CreateZipsInBatches(allFiles)
+	if err != nil {
+		return err
+	}
+
+	listZip, err := ReadZip()
+	if err != nil {
+		return err
+	}
+
+	numFiles := len(listZip)
+
 	noOfWorkers := 5
 	var currentFile int64
-	Jobs := make(chan contracts.FileOps, totalFiles)
 	results := make(chan error, noOfWorkers)
+	Jobs := make(chan contracts.FileOps, numFiles)
 
 	// Create worker goroutines
 	for i := 1; i <= noOfWorkers; i++ {
-		go remote.Worker(Jobs, results, &currentFile, clientUpload, conf, bucket)
+		go Worker(Jobs, results, &currentFile, cfg, bucket, conf.Prefix)
 	}
 
 	bar := progressbar.NewOptions(
-		totalFiles,
+		numFiles,
 		progressbar.OptionSetDescription("Uploading files"),
 		progressbar.OptionShowCount(),
 		progressbar.OptionSetWriter(cmd.F.IOStreams.Out),
@@ -73,47 +75,14 @@ func uploadFiles(f *cmdutil.Factory, conf *contracts.AzionApplicationOptionsV3, 
 		bar = nil
 	}
 
-	if err := cmd.FilepathWalk(pathStatic, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Skip node_modules and .edge directories
-		if info.IsDir() && (strings.Contains(path, "node_modules") || strings.Contains(path, ".edge")) {
-			logger.Debug("Skipping directory: " + path)
-			return filepath.SkipDir
-		}
-
-		if !info.IsDir() {
-			fileContent, err := cmd.Open(path)
-			if err != nil {
-				logger.Debug("Error while trying to read file <"+path+"> about to be uploaded", zap.Error(err))
-				return err
-			}
-
-			fileString := strings.TrimPrefix(path, pathStatic)
-			mimeType, err := mimemagic.MatchFilePath(path, -1)
-			if err != nil {
-				logger.Debug("Error while matching file path", zap.Error(err))
-				return err
-			}
-			fileOptions := contracts.FileOps{
-				Path:        fileString,
-				MimeType:    mimeType.MediaType(),
-				FileContent: fileContent,
-			}
-
-			Jobs <- fileOptions
-		}
-		return nil
-	}); err != nil {
-		logger.Debug("Error while reading files to be uploaded", zap.Error(err))
-		return err
+	for _, f := range listZip {
+		Jobs <- f
 	}
+
 	close(Jobs)
 
 	// Check for errors from workers
-	for a := 1; a <= totalFiles; a++ {
+	for a := 1; a <= numFiles; a++ {
 		result := <-results
 		if result != nil {
 			return result
@@ -133,4 +102,110 @@ func uploadFiles(f *cmdutil.Factory, conf *contracts.AzionApplicationOptionsV3, 
 	*msgs = append(*msgs, msg.UploadSuccessful)
 
 	return nil
+}
+
+func ReadAllFiles(pathStatic string, cmd *DeployCmd) ([]contracts.FileOps, error) {
+	var listFiles []contracts.FileOps
+
+	if err := cmd.FilepathWalk(pathStatic, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Handle symlinks and resolve actual path type
+		resolvedInfo, statErr := os.Stat(path)
+		if statErr != nil {
+			logger.Debug("Error resolving path", zap.String("path", path), zap.Error(statErr))
+			return statErr
+		}
+
+		// Skip directories like node_modules or .edge
+		if resolvedInfo.IsDir() && (strings.Contains(path, "node_modules") || strings.Contains(path, ".edge")) {
+			logger.Debug("Skipping directory: " + path)
+			return filepath.SkipDir
+		}
+
+		// Process files
+		if !resolvedInfo.IsDir() {
+			fileContent, err := cmd.Open(path)
+			if err != nil {
+				logger.Debug("Error while trying to read file <"+path+"> about to be uploaded", zap.Error(err))
+				return err
+			}
+
+			fileString := strings.TrimPrefix(path, pathStatic)
+			mimeType, err := mimemagic.MatchFilePath(path, -1)
+			if err != nil {
+				logger.Debug("Error while matching file path", zap.Error(err))
+				return err
+			}
+
+			fileOptions := contracts.FileOps{
+				Path:        fileString,
+				MimeType:    mimeType.MediaType(),
+				FileContent: fileContent,
+			}
+
+			listFiles = append(listFiles, fileOptions)
+		}
+
+		return nil
+	}); err != nil {
+		logger.Debug("Error while reading files", zap.Error(err))
+		return nil, err
+	}
+
+	return listFiles, nil
+}
+
+// worker reads the range of jobs and uploads the file, if there is an error during upload, we returning it through the results channel
+func Worker(jobs <-chan contracts.FileOps, results chan<- error, currentFile *int64, clientUpload aws.Config, bucket, prefix string) {
+	for job := range jobs {
+		// Once ENG-27343 is completed, we might be able to remove this piece of code
+		fileInfo, err := job.FileContent.Stat()
+		if err != nil {
+			logger.Debug("Error while worker tried to read file stats", zap.Error(err))
+			results <- err
+			return
+		}
+
+		// Check if the file size is zero
+		if fileInfo.Size() == 0 {
+			logger.Debug("\nSkipping upload of empty file: " + job.Path)
+			results <- nil
+			atomic.AddInt64(currentFile, 1)
+			return
+		}
+
+		if err := s3.UploadFile(context.Background(), clientUpload, &job, bucket, prefix); err != nil {
+			logger.Debug("Error while worker tried to upload file: <"+job.Path+"> to storage api", zap.Error(err))
+			for Retries < 5 {
+				atomic.AddInt64(&Retries, 1)
+
+				time.Sleep(time.Second * time.Duration(Retries))
+
+				_, err := job.FileContent.Seek(0, 0)
+				if err != nil {
+					logger.Debug("An error occurred while seeking fileContent", zap.Error(err))
+					break
+				}
+
+				logger.Debug("Retrying to upload the following file: <"+job.Path+"> to storage api", zap.Error(err))
+				err = s3.UploadFile(context.Background(), clientUpload, &job, bucket, prefix)
+				if err != nil {
+					continue
+				}
+				break
+			}
+
+			if Retries >= 5 {
+				logger.Debug("There have been 5 retries already, quitting upload")
+				results <- err
+				return
+			}
+		}
+
+		atomic.AddInt64(currentFile, 1)
+		results <- nil
+	}
 }
