4f850914b711debc4846fba613bfb65af1cc4f4c
PatrickMenoti <patrick.menoti@azion.com>
2026-04-02 13:48:45 -0300

    tests: updated fixtures with created_at field

--- pkg/cmd/create/network_list/network_list_test.go
+++ pkg/cmd/create/network_list/network_list_test.go
@@ -48,6 +48,7 @@ var successResponse string = `
     ],
     "last_editor": "user@example.com",
     "last_modified": "2019-08-24T14:15:22Z",
+    "created_at": "2019-08-24T14:15:22Z",
     "active": true
   }
 }
