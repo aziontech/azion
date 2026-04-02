83b73792afc21672b53f29226cb34f79860ef3d3
PatrickMenoti <patrick.menoti@azion.com>
2026-04-02 14:27:41 -0300

    refactor: update function structure

--- pkg/cmd/list/function/function.go
+++ pkg/cmd/list/function/function.go
@@ -17,13 +17,13 @@ import (
 
 type ListCmd struct {
 	Io            *iostreams.IOStreams
-	ListFunctions func(context.Context, *contracts.ListOptions) (*sdk.PaginatedFunctionsList, error)
+	ListFunctions func(context.Context, *contracts.ListOptions) (*sdk.PaginatedEdgeFunctionList, error)
 }
 
 func NewListCmd(f *cmdutil.Factory) *ListCmd {
 	return &ListCmd{
 		Io: f.IOStreams,
-		ListFunctions: func(ctx context.Context, opts *contracts.ListOptions) (*sdk.PaginatedFunctionsList, error) {
+		ListFunctions: func(ctx context.Context, opts *contracts.ListOptions) (*sdk.PaginatedEdgeFunctionList, error) {
 			client := api.NewClient(f.HttpClient, f.Config.GetString("api_v4_url"), f.Config.GetString("token"))
 			return client.List(ctx, opts)
 		},
