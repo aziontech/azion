b2d954b404b61a35e1917c2bb3a4afe97ed506d5
PatrickMenoti <patrick.menoti@azion.com>
2026-05-29 15:12:11 -0300

    refactor: update message flag

--- messages/list/presets/messages.go
+++ messages/list/presets/messages.go
@@ -3,7 +3,7 @@ package presets
 var (
 	Usage            = "presets"
 	ShortDescription = "Displays the available presets in a list"
-	LongDescription  = "Displays all available presets that can be used to scaffold a project"
+	LongDescription  = "Displays all available presets that can be used to build and deploy a project"
 	HelpFlag         = "Displays more information about the 'list presets' command"
 
 	GettingPresets = "Fetching available presets\n"
